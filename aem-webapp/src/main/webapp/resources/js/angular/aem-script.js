5408cf867f81f0b42e11c63956003af7d9a713fd
z003bpej <gitcentric@siemens.com>
2014-02-04 17:04:38 -0500

    Implement a controller for angular to display data from scope @GitCentric Sync - AccuRev promote transaction #37522

--- aem-webapp/src/main/webapp/resources/js/angular/aem-script.js
+++ aem-webapp/src/main/webapp/resources/js/angular/aem-script.js
@@ -5,9 +5,18 @@ angular.module('aemScript', [])
         templateUrl: 'public-resources/template/main-tabs.html'
     };
 }])
+
+.controller('aemJvmtableCtrl', function($scope) {
+    $scope.jvmItems = [{name: 'CR01_TRE_1', host: 'HOST_XX_1', httpPort: '8080', availableHeap: '400kb', totalHeap: '1024kb'},
+                       {name: 'CR01_TRE_2', host: 'HOST_XX_2', httpPort: '8080', availableHeap: '400kb', totalHeap: '1024kb'}];
+})
+
 .directive('aemJvmtable', [function(){
     return {
         restrict: 'E',
+        scope: {
+            jvmInfos: '=info'
+        },
         templateUrl: 'public-resources/template/jvm-tbl.html',
     };
 }]);
