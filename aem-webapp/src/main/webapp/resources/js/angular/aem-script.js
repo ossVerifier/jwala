ef632471112548c352c74dc2b44d7d7032586d34
z003bpej <gitcentric@siemens.com>
2014-02-05 14:14:59 -0500

    Format long codes to fit 120 characters @GitCentric Sync - AccuRev promote transaction #37652

--- aem-webapp/src/main/webapp/resources/js/angular/aem-script.js
+++ aem-webapp/src/main/webapp/resources/js/angular/aem-script.js
@@ -10,9 +10,12 @@ angular.module('aemScript', [])
 .controller('aemJvmtableCtrl', function($scope) {
     $scope.jvmHeaders = ['JVM', 'Host', 'HTTP/1.1 Port', 'Available Heap', 'Total Heap', 'HTTP Session Count',
                          'HTTP Request Count', 'Group'];
-    $scope.jvmFields = ['name', 'host', 'httpPort', 'availableHeap', 'totalHeap', 'httpSessionCount', 'httpRequestCount', 'group'];
-    $scope.jvmItems = [{name: 'CTO_HC_SRN012_1', host: 'SRN012', httpPort: '8080', availableHeap: '1.2 gb', totalHeap: '3 gb', httpSessionCount: '5', httpRequestCount: '2', group: 'Group 1'},
-                       {name: 'CTO_HC_SRN012_2', host: 'SRN013', httpPort: '8080', availableHeap: '2 gb', totalHeap: '3 gb', httpSessionCount: '2', httpRequestCount: '10', group: 'Group 2'}];
+    $scope.jvmFields = ['name', 'host', 'httpPort', 'availableHeap', 'totalHeap', 'httpSessionCount',
+                        'httpRequestCount', 'group'];
+    $scope.jvmItems = [{name: 'CTO_HC_SRN012_1', host: 'SRN012', httpPort: '8080', availableHeap: '1.2 gb',
+                            totalHeap: '3 gb', httpSessionCount: '5', httpRequestCount: '2', group: 'Group 1'},
+                       {name: 'CTO_HC_SRN012_2', host: 'SRN013', httpPort: '8080', availableHeap: '2 gb',
+                            totalHeap: '3 gb', httpSessionCount: '2', httpRequestCount: '10', group: 'Group 2'}];
 })
 
 .directive('aemJvmtable', [function(){
