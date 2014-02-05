75d6ca5ac02f68b2ebc23e2566d1c78342776caa
z003bpej <gitcentric@siemens.com>
2014-02-05 11:41:08 -0500

    Make tab template conform to controller-scope data definitions @GitCentric Sync - AccuRev promote transaction #37621

--- aem-webapp/src/main/webapp/resources/js/angular/aem-script.js
+++ aem-webapp/src/main/webapp/resources/js/angular/aem-script.js
@@ -7,14 +7,19 @@ angular.module('aemScript', [])
 }])
 
 .controller('aemJvmtableCtrl', function($scope) {
-    $scope.jvmItems = [{name: 'CR01_TRE_1', host: 'HOST_XX_1', httpPort: '8080', availableHeap: '400kb', totalHeap: '1024kb'},
-                       {name: 'CR01_TRE_2', host: 'HOST_XX_2', httpPort: '8080', availableHeap: '400kb', totalHeap: '1024kb'}];
+    $scope.jvmHeaders = ['JVM', 'Host', 'HTTP Port', 'Available Heap', 'Total Heap'];
+    $scope.jvmFields = ['name', 'host', 'httpPort', 'availableHeap', 'totalHeap'];
+    $scope.jvmItems = {item:{name: 'CR01_TRE_1', host: 'HOST_XX_1', httpPort: '8080', availableHeap: '400kb', totalHeap: '1024kb'},
+                       item:{name: 'CR01_TRE_2', host: 'HOST_XX_2', httpPort: '8080', availableHeap: '400kb', totalHeap: '1024kb'}};
+
 })
 
 .directive('aemJvmtable', [function(){
     return {
         restrict: 'E',
         scope: {
+            jvmHeaders: '=headers',
+            jvmFields: '=fields',
             jvmData: '=data'
         },
         templateUrl: 'public-resources/template/jvm-tbl.html',
