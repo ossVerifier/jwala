c801fbc7f0678ac7b3a8208fddc92e848ef0b105
z003bpej <gitcentric@siemens.com>
2014-02-20 02:03:27 -0500

    Add a unit test for RestConfig class @GitCentric Sync - AccuRev promote transaction #40576

---
+++ aem-webservices/src/test/java/com/siemens/cto/aem/ws/config/RestConfigTest.java
@@ -0,0 +1,13 @@
+package com.siemens.cto.aem.ws.config;
+
+import junit.framework.TestCase;
+
+public class RestConfigTest extends TestCase {
+
+    public void testConstructor() {
+        final RestConfigTest restConfig = new RestConfigTest();
+
+        assertNotNull(restConfig);
+    }
+
+}
