9f89bceccc9656cb9f9cb5d7d5afb871f9de35a3
z003bpej <gitcentric@siemens.com>
2014-02-26 15:35:33 -0500

    Remove jvmInfoService bean definition since this was transferred to a spring xml context file @GitCentric Sync - AccuRev promote transaction #41266

--- aem-services/src/main/java/com/siemens/cto/aem/service/configuration/application/ApacheEnterpriseManagerServiceAppConfig.java
+++ aem-services/src/main/java/com/siemens/cto/aem/service/configuration/application/ApacheEnterpriseManagerServiceAppConfig.java
@@ -1,16 +1,8 @@
 package com.siemens.cto.aem.service.configuration.application;
 
-import com.siemens.cto.aem.service.JvmInfoService;
-import com.siemens.cto.aem.service.JvmInfoServiceImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApacheEnterpriseManagerServiceAppConfig {
-
-    @Bean
-    public JvmInfoService jvmInfoService() {
-        return new JvmInfoServiceImpl();
-    }
 
 }
