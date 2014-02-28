ea5197c3075fc2b3863dcb4e0136c609d87f8449
z003bpej <gitcentric@siemens.com>
2014-02-28 18:59:07 -0500

    Add INVALID_POST_DATA status @GitCentric Sync - AccuRev promote transaction #41688

--- aem-webservices/src/main/java/com/siemens/cto/aem/ws/rest/ApplicationResponseStatus.java
+++ aem-webservices/src/main/java/com/siemens/cto/aem/ws/rest/ApplicationResponseStatus.java
@@ -5,7 +5,7 @@ package com.siemens.cto.aem.ws.rest;
  */
 public enum ApplicationResponseStatus {
 
-    SUCCESS, RECORD_NOT_FOUND, RECORD_NOT_ADDED, RECORD_NOT_UPDATED, RECORD_NOT_DELETED;
+    SUCCESS, RECORD_NOT_FOUND, RECORD_NOT_ADDED, RECORD_NOT_UPDATED, RECORD_NOT_DELETED, INVALID_POST_DATA;
 
     public final String getCode() {
         return Integer.toString(this.ordinal());
