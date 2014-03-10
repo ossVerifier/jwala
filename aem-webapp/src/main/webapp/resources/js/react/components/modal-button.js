90a0de57c460b48c4ee064fdf1f75dac62cb5eae
Z003BPEJ <jedd.cuison.ext@siemens.com>
2014-03-10 14:18:45 -0400

    Fix encapsulation problem by using the component's property directly rather than assigning it to a global variable.

--- aem-webapp/src/main/webapp/resources/js/react/components/modal-button.js
+++ aem-webapp/src/main/webapp/resources/js/react/components/modal-button.js
@@ -1,14 +1,10 @@
-var modalDialog;
-
 var ModalButton = React.createClass({
     render: function() {
-        modalDialog = this.props.modalDialog;
         return React.DOM.div(null,
-                              modalDialog,
-                              React.DOM.input({type:"button", onClick:this.handleClick, value:this.props.label}));
+                             this.props.modalDialog,
+                             React.DOM.input({type:"button", onClick:this.handleClick, value:this.props.label}));
     },
     handleClick: function() {
-        modalDialog.show();
+        this.props.modalDialog.show();
     }
-
 });
