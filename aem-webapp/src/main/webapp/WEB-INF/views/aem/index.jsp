0461ea294c5fe667102c6413bed8ed8af5144685
Z003BPEJ <jedd.cuison.ext@siemens.com>
2014-03-11 15:48:20 -0400

    Implement edit and delete JVM info

--- aem-webapp/src/main/webapp/WEB-INF/views/aem/index.jsp
+++ aem-webapp/src/main/webapp/WEB-INF/views/aem/index.jsp
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
+<meta http-equiv="x-ua-compatible" content="IE=Edge"/>
 <title>Apache Enterprise Manager</title>
 <link rel="stylesheet" type="text/css" href="public-resources/css/aem.css">
 <link rel="stylesheet" type="text/css" href="public-resources/css/react/components/default/area.css">
@@ -8,16 +9,20 @@
 <link rel="stylesheet" type="text/css" href="public-resources/css/react/components/default/tabs.css">
 <link rel="stylesheet" type="text/css" href="public-resources/css/react/components/default/data-grid.css">
 <link rel="stylesheet" type="text/css" href="public-resources/css/react/components/default/modal-form-dialog.css">
+<link rel="stylesheet" type="text/css" href="public-resources/css/themes/redmond/jquery-ui-1.10.4.custom.min.css">
 <script src="public-resources/js/react/es5-shim-2.3.0.min.js"></script>
 <script src="public-resources/js/react/es5-sham.min.js"></script>
 <script src="public-resources/js/react/react-0.8.0.min.js"></script>
 <script src="public-resources/js/react/jquery-1.10.0.min.js"></script>
+<script src="public-resources/js/react/jquery-ui-1.10.4.custom.min.js"></script>
 <script src="public-resources/js/json2.min.js"></script>
 <script src="public-resources/js/react/components/area.js"></script>
 <script src="public-resources/js/react/components/tabs.js"></script>
 <script src="public-resources/js/react/components/modal-form-dialog.js"></script>
+<script src="public-resources/js/react/components/modal-form-dialog-jquery-ui.js"></script>
 <script src="public-resources/js/react/components/modal-button.js"></script>
 <script src="public-resources/js/react/components/data-grid.js"></script>
+<script src="public-resources/js/react/components/delete-item-widget.js"></script>
 </head>
 <body>
 
@@ -26,6 +31,18 @@
             template: "public-resources/templates/main-area.html"}),
             document.body);
     </script>
+
+    <!--
+    <script>
+      $(function() {
+        $( "#dialog" ).dialog();
+      });
+    </script>
+
+    <div id="dialog" title="Basic dialog">
+      <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
+    </div>
+    -->
 
 </body>
 </html>
--- aem-webapp/src/main/webapp/resources/js/react/components/data-grid.js
+++ aem-webapp/src/main/webapp/resources/js/react/components/data-grid.js
@@ -1,18 +1,53 @@
 var DataGrid = React.createClass({
-
+    getInitialState: function() {
+        return {data: []};
+    },
+    refresh: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    componentWillMount: function() {
+        this.refresh();
+    },
     render: function() {
-        var jsonData = [{"name":"the-jvm-name-1","id":16,"host":"the-host-name-1","groupInfo":{"name":"the-group-name","id":5}},{"name":"test1","id":19,"host":"test1","groupInfo":{"name":"the-group-name","id":5}},{"name":"test","id":22,"host":"test","groupInfo":{"name":"Group 1","id":2}},{"name":"jvm10","id":23,"host":"host10","groupInfo":{"name":"Group 4","id":7}},{"name":"jvm11","id":24,"host":"host11","groupInfo":{"name":"Group 4","id":7}},{"name":"jvm13","id":27,"host":"host13","groupInfo":{"name":"Group 3","id":9}},{"name":"jvm12","id":28,"host":"jvm12","groupInfo":{"name":"Group 1","id":2}},{"name":"jvm15","id":32,"host":"host15","groupInfo":{"name":"Group 1","id":2}},{"name":"test2","id":36,"host":"test2","groupInfo":{"name":"Group 1","id":2}},{"name":"test7","id":40,"host":"host7","groupInfo":{"name":"Group 1","id":2}},{"name":"test10","id":42,"host":"host10","groupInfo":{"name":"Group 1","id":2}},{"name":"test12","id":45,"host":"host12","groupInfo":{"name":"Group 1","id":2}},{"name":"test13","id":47,"host":"host13","groupInfo":{"name":"Group 3","id":9}},{"name":"test14","id":48,"host":"host14","groupInfo":{"name":"Group 2","id":15}},{"name":"test20","id":49,"host":"host20","groupInfo":{"name":"Group 3","id":9}},{"name":"test21","id":50,"host":"host21","groupInfo":{"name":"Group 2","id":15}},{"name":"test22","id":52,"host":"host22","groupInfo":{"name":"Group 1","id":2}},{"name":"test30","id":54,"host":"host30","groupInfo":{"name":"Group 2","id":15}},{"name":"test31","id":57,"host":"host31","groupInfo":{"name":"Group 2","id":15}},{"name":"test33","id":61,"host":"host33","groupInfo":{"name":"Group 1","id":2}},{"name":"test34","id":62,"host":"host34","groupInfo":{"name":"Group 1","id":2}},{"name":"test35","id":63,"host":"host35","groupInfo":{"name":"Group 1","id":2}},{"name":"test37","id":64,"host":"host37","groupInfo":{"name":"Group 1","id":2}},{"name":"test38","id":66,"host":"test38","groupInfo":{"name":"Group 1","id":2}},{"name":"test39","id":68,"host":"host39","groupInfo":{"name":"Group 1","id":2}},{"name":"test40","id":69,"host":"host40","groupInfo":{"name":"Group 4","id":7}}];
+
+       var jsonData;
+       if (this.state.data.applicationResponseContent !== undefined) {
+            jsonData = this.state.data.applicationResponseContent.content;
+       } else {
+            jsonData = this.state.data;
+       }
+
         var rows = new Array();
         for (var i = 0; i < jsonData.length ; i++) {
-            rows[i] = Row({data:jsonData[i]});
+           rows[i] = Row({data:jsonData[i], editDialog:this.props.editDialog, thisGrid:this});
         }
 
         // The table has to be in a DIV so that css will work if this component is placed in
         // another table.
         return React.DOM.div({className:"dataGrid-" + this.props.theme},
+                             this.props.editDialog,
                              React.DOM.table({className:"dataGrid-" + this.props.theme},
                              Header({header:this.props.header}),
                              rows));
+    },
+    deleteClick: function() {
+        $("input:checkbox").each(function(i, obj) {
+            if ($(this).is(":checked")) {
+                dialogConfirm.show($(this).attr("value"));
+
+                $("#jvmName").val("test");
+            }
+        });
+
     }
 
 });
@@ -39,12 +74,23 @@ var Row = React.createClass({
         var jsonData = this.props.data;
         var cols = new Array();
         var idx = 0;
-
+        var id;
+        var editDialog = this.props.editDialog;
+        var thisGrid = this.props.thisGrid;
         $.each(jsonData, function(i, val) {
-            cols[idx++] = Column({value:val});
+            if (i === "id") {
+                id = val;
+            }
+
+            // Custom code...
+            if (idx == 0) {
+                cols[idx++] = Column({value:val, type:"link", editDialog:editDialog, thisGrid:thisGrid});
+            } else {
+                cols[idx++] = Column({value:val});
+            }
         });
 
-        return React.DOM.tr(null, Column({value:React.DOM.input({type:"checkbox"})}), cols)
+        return React.DOM.tr(null, Column({value:Checkbox({value:id})}), cols)
     }
 
 });
@@ -52,7 +98,66 @@ var Row = React.createClass({
 var Column = React.createClass({
 
     render: function() {
-        return React.DOM.td(null, this.props.value)
+        if (this.props.type === undefined) {
+            return React.DOM.td(null, this.props.value);
+        } else if (this.props.type === "link") {
+            return React.DOM.td(null, Link({value:this.props.value,
+                                            editDialog:this.props.editDialog, thisGrid:this.props.thisGrid}));
+        }
     }
 
 });
+
+var Link = React.createClass({
+    render: function() {
+        var linkStyle = {"text-decoration":"underline", "background":"none", "color":"blue"};
+        return React.DOM.a({href:"#", style:linkStyle, onClick:this.linkClick}, this.props.value);
+    },
+    linkClick: function() {
+        var editDialog = this.props.editDialog;
+        var thisGrid = this.props.thisGrid;
+        $.getJSON("v1.0/jvm?name=" + this.props.value,
+            function(data) {
+
+                var filteredJson = {};
+
+                var nameMap = {};
+                nameMap["id"] = "jvmId";
+                nameMap["name"] = "jvmNameId";
+                nameMap["host"] = "hostNameId";
+
+                // Custom code...
+                // TODO: Callback json decorator/parser
+
+                $.each(data.applicationResponseContent.content, function(i, obj) {
+                    if (nameMap[i] !== undefined) {
+                          filteredJson[nameMap[i]] = obj;
+                    }
+                });
+                console.log(JSON.stringify(filteredJson));
+                editDialog.show(filteredJson, function(){
+                    thisGrid.refresh();
+                });
+
+         });
+
+    }
+});
+
+var Checkbox = React.createClass({
+    render: function() {
+        return React.DOM.input({type:"checkbox", value:this.props.value, onClick:this.checkboxClicked})
+    },
+    /**
+     * Allow only one item to be checked.
+     */
+    checkboxClicked: function() {
+        var id = this.props.value;
+        $("input:checkbox").each(function(i, obj) {
+            if ($(this).attr("value") != id) {
+                $(this).prop("checked", false);
+            }
+        });
+    }
+});
+
