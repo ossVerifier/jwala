ae01fd9d8c8126c4a25a36d72231ff92853f416d
Jedd Cuison <jedd.cuison.ext@siemens.com>
2014-04-15 14:37:06 -0400

    Add unit tests

--- aem-webapp/src/test/java/com/siemens/cto/aem/web/controller/IndexControllerTest.java
+++ aem-webapp/src/test/java/com/siemens/cto/aem/web/controller/IndexControllerTest.java
@@ -1,6 +1,14 @@
 package com.siemens.cto.aem.web.controller;
 
 import junit.framework.TestCase;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class IndexControllerTest extends TestCase {
     final IndexController ic = new IndexController();
@@ -11,5 +19,40 @@ public class IndexControllerTest extends TestCase {
 
     public void testAbout() {
         assertEquals("aem/about", ic.about());
+    }
+
+    public void testSandbox() {
+        assertEquals("aem/sandbox", ic.sandbox());
+    }
+
+    public void testScripts() {
+        String result = ic.scripts("true", false);
+        assertEquals("aem/dev-scripts", result);
+        result = ic.scripts("true", true);
+        assertEquals("aem/dev-scripts", result);
+        result = ic.scripts("false", false);
+        assertEquals("aem/prod-scripts", result);
+        result = ic.scripts("false", true);
+        assertEquals("aem/prod-scripts", result);
+        result = ic.scripts(null, true);
+        assertEquals("aem/dev-scripts", result);
+        result = ic.scripts(null, false);
+        assertEquals("aem/prod-scripts", result);
+    }
+
+    public void testDevModeTrue() {
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        ModelAndView mv = ic.devMode("true", resp);
+        verify(resp).addCookie(any(Cookie.class));
+        assertNotNull(mv);
+        assertEquals("{devMode=true}", mv.getModel().toString());
+    }
+
+    public void testDevModeFalse() {
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        ModelAndView mv = ic.devMode("false", resp);
+        verify(resp).addCookie(any(Cookie.class));
+        assertNotNull(mv);
+        assertEquals("{devMode=false}", mv.getModel().toString());
     }
 }
