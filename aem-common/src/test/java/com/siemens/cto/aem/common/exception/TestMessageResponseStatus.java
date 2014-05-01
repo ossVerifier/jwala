ea9beb6e04e76393fd8a7c352fea3bc4d2a124a1
meleje00 <gitcentric@siemens.com>
2014-05-01 13:53:49 -0400

    Added tests, removed obsolete class, added or changed serialVersionUID to the initial lvalue of 1 to all exceptions. @GitCentric Sync - AccuRev promote transaction #51368

---
+++ aem-common/src/test/java/com/siemens/cto/aem/common/exception/TestMessageResponseStatus.java
@@ -0,0 +1,13 @@
+package com.siemens.cto.aem.common.exception;
+
+final class TestMessageResponseStatus implements MessageResponseStatus {
+    @Override
+    public String getMessageCode() {
+        return null;
+    }
+
+    @Override
+    public String getMessage() {
+        return null;
+    }
+}
