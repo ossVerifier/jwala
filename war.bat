9d40ebc62b6492e32d3ac0c94772e081705938bc
Peter Horsfield <peter.horsfield@siemens.com>
2014-05-27 13:59:24 -0400

    Make war deployment relative (Peter's command line scripts)

--- war.bat
+++ war.bat
@@ -1,10 +1,11 @@
 @echo off
 call d:\java\setenv.bat
-pushd E:\TOC\v1\git\cto-aem\aem-webapp
+pushd %~dp0\aem-webapp
 call gradle war
 if errorlevel 1 goto fail
-rmdir /s /q E:\TOC\v1\deploy\instances\tc1\webapps\aem
-copy /Y build\libs\aem-webapp-1.0-SNAPSHOT.war E:\TOC\v1\deploy\webapps
+del /Q ..\..\..\aem-webapp-1.0-SNAPSHOT.war 
+rmdir /s /q ..\..\..\deploy\instances\tc1\webapps\aem
+copy /Y build\libs\aem-webapp-1.0-SNAPSHOT.war ..\..\..\webapps
 goto :eof
 :fail
 echo BUILD FAILURE - NO UPDATE
