286b339ea1dc24a9c62a395328bf3a743af58985
Peter Horsfield <peter.horsfield@siemens.com>
2014-05-30 01:49:57 -0400

    file upload

--- war.bat
+++ war.bat
@@ -3,9 +3,10 @@ call d:\java\setenv.bat
 pushd %~dp0\aem-webapp
 call gradle war
 if errorlevel 1 goto fail
-del /Q ..\..\..\aem-webapp-1.0-SNAPSHOT.war 
+del /Q ..\..\..\deploy\webapps\aem-webapp-1.0-SNAPSHOT.war 
 rmdir /s /q ..\..\..\deploy\instances\tc1\webapps\aem
-copy /Y build\libs\aem-webapp-1.0-SNAPSHOT.war ..\..\..\webapps
+copy /Y build\libs\aem-webapp-1.0-SNAPSHOT.war ..\..\..\deploy\webapps
+popd
 goto :eof
 :fail
 echo BUILD FAILURE - NO UPDATE
