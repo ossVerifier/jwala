0c27bdca9271bc35afc46e1ca9fb7a958082851f
Jedd Cuison <jedd.cuison.ext@siemens.com>
2014-06-09 13:39:19 -0400

    Arrange gear position vertically so that it is centered to the word beside it

--- aem-webapp/src/main/webapp/resources/js/react/login-area.js
+++ aem-webapp/src/main/webapp/resources/js/react/login-area.js
@@ -2,22 +2,24 @@
 var LoginArea = React.createClass({
     render: function() {
         return <div className={this.props.className}>
-            <form method="post" action="j_security_check">
-                <br/>
-                <MessageLabel msg={this.props.error} className="login-error-msg"/>
-                <span className="title-2">TOMCAT</span><br/>
-                <img src="public-resources/img/react/gear.gif"/>
-                <span className="title-2">PERATIONS</span><br/>
-                <span className="title-2">CENTER</span>
-                <br/>
-                <br/>
-                <TextBox id="user" name="j_username" className="input" hint="User Name" hintClassName="hint"/>
-                <br/>
-                <TextBox id="pwd" name="j_password" isPassword={true} className="input" hint="Password" hintClassName="hint"/>
-                <br/>
-                <input type="submit" value="Log In" />
-            </form>
-        </div>
+                    <form method="post" action="j_security_check">
+                        <br/>
+                        <MessageLabel msg={this.props.error} className="login-error-msg"/>
+                        <span className="title-2">TOMCAT</span><br/>
+                        <div className="gear-position">
+                            <img src="public-resources/img/react/gear.gif"/>
+                        </div>
+                        <span className="title-2">OPERATIONS</span><br/>
+                        <span className="title-2">CENTER</span>
+                        <br/>
+                        <br/>
+                        <TextBox id="user" name="j_username" className="input" hint="User Name" hintClassName="hint"/>
+                        <br/>
+                        <TextBox id="pwd" name="j_password" isPassword={true} className="input" hint="Password" hintClassName="hint"/>
+                        <br/>
+                        <input type="submit" value="Log In" />
+                    </form>
+              </div>
     }
 });
 
