3c272fb4c06b95147985ff3b15efae2485f6d411
Peter Horsfield <peter.horsfield@siemens.com>
2014-06-18 14:54:09 -0400

    Update peters paths to match my V drive.

--- copywebcontent.bat
+++ copywebcontent.bat
@@ -1,6 +1,10 @@
 @echo off
+SETLOCAL
+SET STP_HOME=\apache
+SET STP_TC_HOME=%STP_HOME%\tomcat
 pushd %~dp0
-robocopy aem-webapp\src\main\webapp\resources ..\..\deploy\instances\tc1\webapps\aem\resources /XF web.xml /XF context.xml /XD lib /MIR
-robocopy aem-webapp\src\main\webapp\WEB-INF\views ..\..\deploy\instances\tc1\webapps\aem\WEB-INF\views /XF web.xml /XF context.xml /XD lib /MIR
-robocopy aem-webapp\build\react\js ..\..\deploy\instances\tc1\webapps\aem\gen\resources\js\react /E
+robocopy aem-webapp\src\main\webapp\resources %STP_TC_HOME%\instances\tc1\webapps\aem\resources /XF web.xml /XF context.xml /XD lib /MIR
+robocopy aem-webapp\src\main\webapp\WEB-INF\views %STP_TC_HOME%\instances\tc1\webapps\aem\WEB-INF\views /XF web.xml /XF context.xml /XD lib /MIR
+robocopy aem-webapp\build\react\js %STP_TC_HOME%\instances\tc1\webapps\aem\gen\resources\js\react /E
 popd
+ENDLOCAL
--- war.bat
+++ war.bat
@@ -1,11 +1,14 @@
 @echo off
+SETLOCAL
 call d:\java\setenv.bat
+SET STP_HOME=\apache
+SET STP_TC_HOME=%STP_HOME%\tomcat
 pushd %~dp0\aem-webapp
 call gradle war
 if errorlevel 1 goto fail
-del /Q ..\..\..\deploy\webapps\aem-webapp-1.0-SNAPSHOT.war 
-rmdir /s /q ..\..\..\deploy\instances\tc1\webapps\aem
-copy /Y build\libs\aem-webapp-1.0-SNAPSHOT.war ..\..\..\deploy\webapps
+del /Q %STP_TC_HOME%\webapps\aem-webapp-1.0-SNAPSHOT.war 
+rmdir /s /q %STP_TC_HOME%\instances\tc1\webapps\aem
+copy /Y build\libs\aem-webapp-1.0-SNAPSHOT.war %STP_TC_HOME%\webapps
 popd
 goto :eof
 :fail
