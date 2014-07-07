9f09f359847cc810b33020ca05b2f2df9529e799
Jedd Cuison <jedd.cuison.ext@siemens.com>
2014-07-07 01:53:34 -0400

    Fix bug wherein the text box hint is not hidden when the browser auto-fills it in a "remember me" via browser scenario.

--- aem-webapp/src/main/webapp/resources/js/react/generic-components/textbox.js
+++ aem-webapp/src/main/webapp/resources/js/react/generic-components/textbox.js
@@ -27,18 +27,43 @@ var TextBox = React.createClass({
                            onChange={this.handleChange}/>
                 </div>
     },
+    /**
+     * This method is needed to hide the label and set the state if the
+     * browser filled this component with a saved value automatically.
+     * This is primarily used to handle browser "remember me" log in
+     * credentials.
+     */
+    checkForBrowserSavedInput: function() {
+        var val = $(this.getDOMNode().children[1]).val();
+        if (val !== "") {
+            this.setComponentState(val);
+        }
+    },
+    componentDidMount: function() {
+        /**
+         * The setTimeout is needed since the browser auto-fill event seems
+         * to happen after componentDidMount life cycle. In addition, no html
+         * input event fires on auto-fill thus the need for a method to be
+         * executed at a later time to check if this component was filled up
+         * by the browser.
+         */
+        setTimeout(this.checkForBrowserSavedInput, 100);
+    },
     handleBlur: function() {
         if (this.state.value === "") {
             this.setState({inputClassName:"input-on-blur " + this.props.className});
         }
     },
     handleChange: function(event) {
+        this.setComponentState(event.target.value);
+    },
+    setComponentState: function(val) {
         var className;
-        if (event.target.value === "") {
+        if (val === "") {
             className = "input-on-blur " + this.props.className;
         } else {
             className = "input-on-focus " + this.props.className;
         }
-        this.setState({inputClassName: className, value: event.target.value});
+        this.setState({inputClassName: className, value: val});
     }
 })
