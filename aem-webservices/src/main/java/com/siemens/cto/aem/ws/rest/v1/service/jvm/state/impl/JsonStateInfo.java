b721192dce973adbec0c50f7c5838978d7d5d4d7
Peter Horsfield <peter.horsfield@siemens.com>
2014-07-21 14:27:07 -0400

    Another temporary commit.

---
+++ aem-webservices/src/main/java/com/siemens/cto/aem/ws/rest/v1/service/jvm/state/impl/JsonStateInfo.java
@@ -0,0 +1,37 @@
+package com.siemens.cto.aem.ws.rest.v1.service.jvm.state.impl;
+
+import java.util.Set;
+
+import org.joda.time.DateTime;
+
+import com.siemens.cto.aem.domain.model.group.Group;
+import com.siemens.cto.aem.domain.model.group.GroupState;
+import com.siemens.cto.aem.domain.model.id.Identifier;
+import com.siemens.cto.aem.domain.model.jvm.CurrentJvmState;
+
+/**
+ * Intended for serialization of state notifications
+ */
+public class JsonStateInfo {
+
+    public class JsonGroupState {
+
+        final Identifier<Group>   id;
+        final GroupState          state;
+        final DateTime            asOf;
+        
+        public JsonGroupState(Group group) {
+            id = group.getId();
+            state = group.getState();
+            asOf = group.getAsOf();
+        }
+    }
+    
+    final Set<CurrentJvmState>    jvms;
+    final Set<JsonGroupState>     groups;
+    
+    public JsonStateInfo(final Set<CurrentJvmState> jvms, final Set<JsonGroupState> groups) {
+        this.jvms =  jvms;
+        this.groups = groups;
+    }
+}
