bde5947a4875fc368276ae1e6c03e541f14880f1
Corey Gantzhorn <corey.gantzhorn.ext@siemens.com>
2014-07-24 11:16:17 -0400

    Javascript cleanup for state-related functionality.

--- aem-webapp/src/main/webapp/resources/js/toc/v1/service/stateService.js
+++ aem-webapp/src/main/webapp/resources/js/toc/v1/service/stateService.js
@@ -2,6 +2,16 @@ var stateService = function() {
 
     var clientId = Date.now();
 
+    var constructIdParameters = function(type, ids) {
+        if (ids === undefined) {
+            return "";
+        } else {
+            var paramIds = {};
+            paramIds[type] = ids;
+            return "?" + $.param(paramIds);
+        }
+    };
+
     var addTimeoutParameter = function(timeout, params) {
         if (timeout !== undefined) {
             params.timeout = timeout;
@@ -49,6 +59,14 @@ var stateService = function() {
                 stop: function() { shouldKeepGoing = false;},
                 consume: consumeFunc
             };
+        },
+        getCurrentJvmStates : function(ids) {
+            return serviceFoundation.promisedGet("v1.0/jvms/states/current" + constructIdParameters("jvmId", ids),
+                                                 "json");
+        },
+        getCurrentWebServerStates : function(ids) {
+            return serviceFoundation.promisedGet("v1.0/webservers/states/current" + constructIdParameters("webServerId", ids),
+                                                 "json");
         }
     };
 }();
