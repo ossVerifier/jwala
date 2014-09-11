902d0e591611bb96ec081923d87d4028ee794244
Peter Horsfield <peter.horsfield@siemens.com>
2014-09-11 15:36:30 -0400

    Send entire exception trace back for UI to display on request.

--- aem-services/src/main/java/com/siemens/cto/aem/service/state/impl/AbstractStateServiceFacade.java
+++ aem-services/src/main/java/com/siemens/cto/aem/service/state/impl/AbstractStateServiceFacade.java
@@ -1,10 +1,15 @@
 package com.siemens.cto.aem.service.state.impl;
 
-import com.siemens.cto.aem.domain.model.state.OperationalState;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import org.joda.time.DateTime;
 
+import com.siemens.cto.aem.common.exception.ExceptionUtil;
 import com.siemens.cto.aem.domain.model.id.Identifier;
 import com.siemens.cto.aem.domain.model.state.CurrentState;
+import com.siemens.cto.aem.domain.model.state.OperationalState;
 import com.siemens.cto.aem.domain.model.state.StateType;
 import com.siemens.cto.aem.domain.model.state.command.SetStateCommand;
 import com.siemens.cto.aem.domain.model.temporary.User;
@@ -31,14 +36,60 @@ public abstract class AbstractStateServiceFacade<S, T extends OperationalState> 
         setState(newCurrentState);
     }
 
+    @Deprecated
     public void setStateWithMessage(final Identifier<S> anId,
-                                    final T aNewState,
-                                    final DateTime anAsOf,
-                                    final String aMessage) {
+            final T aNewState,
+            final DateTime anAsOf,
+            final String aMessage) {
         final CurrentState<S, T> newCurrentState = createCurrentStateWithMessage(anId,
-                                                                                 aNewState,
-                                                                                 anAsOf,
-                                                                                 aMessage);
+                                                                 aNewState,
+                                                                 anAsOf,
+                                                                 aMessage);
+        setState(newCurrentState);
+    }
+
+    public void setStateWithMessageAndException(final Identifier<S> anId,
+            final T aNewState,
+            final DateTime anAsOf,
+            final String aMessage,
+            final Throwable anException) throws IOException /*does not really.*/ {
+
+        StringWriter writer = null;
+        PrintWriter printer = null;
+        Throwable penaCause = null;
+        if(anException != null) {
+            penaCause = ExceptionUtil.INSTANCE.getPenultimateRootCause(anException);
+            if(penaCause == null) { 
+                penaCause = anException;
+            }
+            writer = new StringWriter();
+            printer = new PrintWriter(writer);
+        } 
+        
+        String message = "";
+        if(penaCause == null && aMessage == null) { 
+            message = "";
+        } else if(aMessage == null && penaCause != null) { 
+            printer.append(penaCause.getLocalizedMessage());
+        } else if(penaCause != null) {
+            printer.append(aMessage);
+        } else { 
+            message = aMessage;
+        }
+        
+        if(penaCause != null) {
+            printer.append(System.lineSeparator());
+            penaCause.printStackTrace(printer);
+            printer.flush();
+            message = writer.toString();
+            printer.close();
+            writer.close();
+        }
+        
+        final CurrentState<S, T> newCurrentState = createCurrentStateWithMessage(anId,
+                                                                 aNewState,
+                                                                 anAsOf,
+                                                                 message);
         setState(newCurrentState);
     }
 
