192b2401b4d3b181ded86ae60439c80274eb8cd6
Peter Horsfield <peter.horsfield@siemens.com>
2014-10-24 01:18:38 -0400

    Truncate status message heading to logs to 1024bytes. Fix UNREACHABLE in webserver-heartbeat-integration.xml

--- aem-domain/src/main/java/com/siemens/cto/aem/domain/model/state/CurrentState.java
+++ aem-domain/src/main/java/com/siemens/cto/aem/domain/model/state/CurrentState.java
@@ -130,7 +130,7 @@ public class CurrentState<S extends Object, T extends OperationalState> implemen
                 .append("state", state)
                 .append("asOf", asOf)
                 .append("type", type)
-                .append("message", (message != null && message.length() > 19) ? message.substring(0, 19) : message)
+                .append("message", (message != null && message.length() > 1024) ? message.substring(0, 1024) : message)
                 .toString();
     }
 }
