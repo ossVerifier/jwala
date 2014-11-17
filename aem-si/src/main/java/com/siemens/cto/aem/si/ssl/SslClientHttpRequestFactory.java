571e9fba3834b183bfab17c3144d9cc9a9d08293
Peter Horsfield <peter.horsfield@siemens.com>
2014-11-17 13:53:55 -0500

    Save an SSL engine to avoid conflict with Tibco EMS SSL

--- aem-si/src/main/java/com/siemens/cto/aem/si/ssl/SslClientHttpRequestFactory.java
+++ aem-si/src/main/java/com/siemens/cto/aem/si/ssl/SslClientHttpRequestFactory.java
@@ -9,6 +9,7 @@ import java.security.SecureRandom;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -17,12 +18,16 @@ public class SslClientHttpRequestFactory extends SimpleClientHttpRequestFactory 
 
     private HostnameVerifier verifier;
     private final SSLContext context;
+    private final SSLSocketFactory socketFactory;
     
-    public SslClientHttpRequestFactory() throws KeyManagementException, NoSuchAlgorithmException { 
-        context = SSLContext.getInstance("TLS");
+    public SslClientHttpRequestFactory() throws KeyManagementException, NoSuchAlgorithmException {
         
+        context = SSLContext.getInstance("TLS");
+
         TrustManager[] tm = { new TrustingX509TrustManager() };
         context.init(null,  tm, new SecureRandom());
+
+        socketFactory = context.getSocketFactory(); 
     }
     
     @Override
@@ -31,7 +36,7 @@ public class SslClientHttpRequestFactory extends SimpleClientHttpRequestFactory 
         if(connection instanceof HttpsURLConnection) { 
             HttpsURLConnection httpsConnection = (HttpsURLConnection)connection;
             httpsConnection.setHostnameVerifier(verifier);
-            httpsConnection.setSSLSocketFactory(context.getSocketFactory());
+            httpsConnection.setSSLSocketFactory(socketFactory);
         }
         
         super.prepareConnection(connection, httpMethod);
