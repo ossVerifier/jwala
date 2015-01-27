dbc81a598545119839541c7bb4fcee6d3567035f
Peter Horsfield <peter.horsfield@siemens.com>
2015-01-27 15:25:32 -0500

    Fix broken test add new one.

--- toc-filemanager/src/test/java/com/siemens/cto/toc/files/TemplateManagerTest.java
+++ toc-filemanager/src/test/java/com/siemens/cto/toc/files/TemplateManagerTest.java
@@ -2,12 +2,16 @@ package com.siemens.cto.toc.files;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Collection;
 import java.util.Properties;
 
 import org.junit.Test;
@@ -19,9 +23,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import com.siemens.cto.aem.domain.model.app.Application;
+import com.siemens.cto.aem.domain.model.resource.ResourceType;
 import com.siemens.cto.toc.files.impl.LocalFileSystemRepositoryImpl;
 import com.siemens.cto.toc.files.impl.PropertyFilesConfigurationImpl;
 import com.siemens.cto.toc.files.impl.TemplateManagerImpl;
+import com.siemens.cto.toc.files.resources.ResourceTypeDeserializer;
 
 
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = {
@@ -44,12 +50,18 @@ public class TemplateManagerTest {
         }
         
         @Bean FilesConfiguration getFilesConfiguration() throws IOException {
-            Path storageFolder = Files.createTempDirectory("archives");
+            Path storageFolder = Files.createTempDirectory("archives");            
             
             Properties p = new Properties();
             p.put(TocPath.TEMPLATES.getProperty(), storageFolder.toString());
+            p.put(TocPath.RESOURCE_TYPES.getProperty(), storageFolder.toString());
 
             return new PropertyFilesConfigurationImpl(p);
+        }
+
+        @Bean
+        public ResourceTypeDeserializer getResourceTypeDeserializer() { 
+            return new ResourceTypeDeserializer();
         }
     }
     
@@ -68,7 +80,22 @@ public class TemplateManagerTest {
     // Managed by setup/teardown
     ByteArrayInputStream uploadedFile;
     Application app;
+
+    public void testGetResourceTypes() throws IOException { 
+       
+       Path storageFolder = filesConfiguration.getConfiguredPath(TocPath.WEB_ARCHIVE);
+       try(BufferedWriter writer = Files.newBufferedWriter(storageFolder.resolve("ResourceProperties.json"), Charset.defaultCharset(), StandardOpenOption.CREATE)) {
+           
+        writer.write("{\"name\":\"MySql XA Database\",\"contentType\":\"application/xml\", \"properties\":[{\"name\":\"\u0032\", \"meta1\":\"meta1\"},{\"name\":\"name two\", \"meta2\" :\"meta two\"}]}");
+
+        Collection<ResourceType> rtypes = templateManager.getResourceTypes();
+        assertEquals(1, rtypes.size());
+        assertEquals("MySql XA Database", rtypes.iterator().next().getName());
         
+       }
+       
+    }
+    
     @Test
     public void testFindCurrent() throws IOException {
         String  result = templateManager.getAbsoluteLocation(new TocFile()  {
