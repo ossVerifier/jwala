8be6dfa109cc2e1cacd406b11573f07b2f7002ab
Peter Horsfield <peter.horsfield@siemens.com>
2015-01-30 16:57:35 -0500

    Integration test framework

---
+++ toc-systest/resources/install-service.bat
@@ -0,0 +1,20 @@
+REM %1 - Location of setenv.bat
+REM %2 - Tomcat core or CATALINA_HOME
+REM %3 - JVM name e.g. JVM-1
+REM %4 - Jacoco agent location
+REM %5 - Jacoco exec destination file e.g. D:\scratch\jacoco\hct-rest.exec
+
+CALL %1\setenv.bat
+
+ECHO Uninstall the service...
+CMD /C %2\bin\service.bat remove %3
+
+ECHO Install the service...
+CMD /C %2\bin\service.bat install %3
+
+ECHO Set java opts...
+
+CMD /C %2\bin\tomcat7 //US//%3 ++JvmOptions %JAVA_SERVICE_OPTS%#-javaagent:%4=destfile=%5,append=true,includes=com.siemens.cto.*
+
+ECHO testing
+ECHO Finished installing %3 as a service
