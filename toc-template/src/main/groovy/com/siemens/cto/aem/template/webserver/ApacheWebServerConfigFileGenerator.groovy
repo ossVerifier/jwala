9795aa7f12b45e73d800322b7863215bc0a462d3
Peter Horsfield <peter.horsfield@siemens.com>
2015-03-23 16:17:38 -0400

    Allow all app information to flow to toc-template templates, allowing jvm selection.

--- toc-template/src/main/groovy/com/siemens/cto/aem/template/webserver/ApacheWebServerConfigFileGenerator.groovy
+++ toc-template/src/main/groovy/com/siemens/cto/aem/template/webserver/ApacheWebServerConfigFileGenerator.groovy
@@ -33,8 +33,8 @@ public class ApacheWebServerConfigFileGenerator {
                                       ) {
         final binding = [webServerName:webServerName,
                          webServer:webServer,
-                         apps:apps.collect {app:[mount: it.webAppContext, name: it.name]},
-                         jvms:jvms.collect {jvm:it},
+                         apps:apps,
+                         jvms:jvms,
                          comments:""]
         return bindDataToTemplate(binding, templateFileName).toString()
     }
