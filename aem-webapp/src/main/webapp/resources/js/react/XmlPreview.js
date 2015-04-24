b55a87e52fc47ba615aac68a248604d0bf4d03fb
Jedd Cuison <jedd.cuison.ext@siemens.com>
2015-04-24 01:26:54 -0400

    Fix XML line feed and indentation pretty print in IE8.

--- aem-webapp/src/main/webapp/resources/js/react/XmlPreview.js
+++ aem-webapp/src/main/webapp/resources/js/react/XmlPreview.js
@@ -7,7 +7,10 @@ var XmlPreview = React.createClass({
         return <div ref="theXmlContainer" className="xml-preview-container"><pre className="theXml"></pre></div>
     },
     componentDidUpdate: function() {
-        var escaped = this.state.content.replace(/</g, "&lt").replace(/>/g, "&gt");
+        // The code replace(/\n/g, "</li><li>") fixes bug wherein regex replace for \n doesn't work for snippet for
+        // IE8.
+        // TODO: Put the fix in snippet and submit as a bug fix for the said open source plugin.
+        var escaped = this.state.content.replace(/</g, "&lt").replace(/>/g, "&gt").replace(/ /g, "&nbsp;").replace(/\n/g, "</li><li>");
         $(this.refs.theXmlContainer.getDOMNode()).html('<pre class="theXml"></pre>');
         $("pre.theXml").html(escaped);
         var style = "";
@@ -17,6 +20,6 @@ var XmlPreview = React.createClass({
         $("pre.theXml").snippet("xml",{style:style, showNum:false});
     },
     refresh: function(content) {
-        this.setState({content:vkbeautify.xml(content)});
+        this.setState({content:vkbeautify.xml(content, "    ")});
     }
 })
