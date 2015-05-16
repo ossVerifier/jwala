e9ae867a5d4cf6063f40e91bb92d5296054633ed
Peter Horsfield <peter.horsfield@siemens.com>
2015-05-16 01:28:10 -0400

    Make test pass no matter gradle or Eclipse. Hope it's ok in intelliJ !

--- aem-control/src/test/java/com/siemens/cto/aem/control/jvm/command/impl/DefaultJvmExecCommandBuilderImplTest.java
+++ aem-control/src/test/java/com/siemens/cto/aem/control/jvm/command/impl/DefaultJvmExecCommandBuilderImplTest.java
@@ -5,23 +5,48 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.FileNotFoundException;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.siemens.cto.aem.common.AemConstants;
+import com.siemens.cto.aem.common.ApplicationException;
+import com.siemens.cto.aem.common.properties.ApplicationProperties;
 import com.siemens.cto.aem.domain.model.exec.ExecCommand;
 import com.siemens.cto.aem.domain.model.jvm.Jvm;
 import com.siemens.cto.aem.domain.model.jvm.JvmControlOperation;
 
 public class DefaultJvmExecCommandBuilderImplTest {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(DefaultJvmExecCommandBuilderImplTest.class);
+    
     private Jvm jvm;
     private DefaultJvmExecCommandBuilderImpl impl;
     private String jvmName;
+    String originalPRP = null;
 
+    @After
+    public void tearDown() {
+        if(originalPRP != null) { 
+            System.setProperty(AemConstants.PROPERTIES_ROOT_PATH, originalPRP);            
+        }
+    }
     @Before
     public void setup() {
-        System.setProperty(AemConstants.PROPERTIES_ROOT_PATH, "src/test/resources");
+        originalPRP = System.getProperty(AemConstants.PROPERTIES_ROOT_PATH);
+        System.setProperty(AemConstants.PROPERTIES_ROOT_PATH, "aem-control/src/test/resources");
+        try {
+            ApplicationProperties.getInstance();
+        } catch( ApplicationException e) { 
+            LOGGER.trace("Attempting to load properties without project in path", e);
+            System.setProperty(AemConstants.PROPERTIES_ROOT_PATH, "src/test/resources");
+            ApplicationProperties.getInstance();            
+        }
+        
         impl = new DefaultJvmExecCommandBuilderImpl();
         jvm = mock(Jvm.class);
         jvmName = "theJvmName";
