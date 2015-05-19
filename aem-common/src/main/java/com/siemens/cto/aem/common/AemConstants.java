e50727a3834c9454236bad10129352fafc22b405
Jedd Cuison <jedd.cuison.ext@siemens.com>
2015-05-19 13:20:45 -0400

    Change STP_PROPERTIES_ROOT_PATH to STP_PROPERTIES_DIR.

--- aem-common/src/main/java/com/siemens/cto/aem/common/AemConstants.java
+++ aem-common/src/main/java/com/siemens/cto/aem/common/AemConstants.java
@@ -2,7 +2,7 @@ package com.siemens.cto.aem.common;
 
 public class AemConstants {
 
-    public static final String PROPERTIES_ROOT_PATH = "STP_PROPERTIES_ROOT_PATH";
+    public static final String PROPERTIES_ROOT_PATH = "STP_PROPERTIES_DIR";
     public static final String PROPERTIES_FILE_NAME = "toc.properties";
 
 }
