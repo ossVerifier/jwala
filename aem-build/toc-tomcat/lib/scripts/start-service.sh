985c0bef587858eee747eb6c54f99b08dea3a16a
z0033r5b <gitcentric@siemens.com>
2015-06-25 10:50:18 -0400

    update exit codes @GitCentric Sync - AccuRev promote transaction #124647

--- aem-build/toc-tomcat/lib/scripts/start-service.sh
+++ aem-build/toc-tomcat/lib/scripts/start-service.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+STP_EXIT_CODE_NO_SUCH_SERVICE=123
 STP_EXIT_CODE_TIMED_OUT=124
+STP_EXIT_CODE_USAGE=125
 STP_EXIT_CODE_ABNORMAL_SUCCESS=126
 STP_EXIT_CODE_NO_OP=127
 STP_EXIT_CODE_SUCCESS=0
@@ -8,14 +10,14 @@ STP_EXIT_CODE_FAILED=1
 
 if [ "$1" = "" -o "$2" = "" ]; then
     echo $0 not invoked with service name or time out in seconds
-    exit 125;
+    exit $STP_EXIT_CODE_USAGE;
 fi
 
 export JVMINST=`sc queryex $1 | head -1 | awk '{ sub(/:/,"",$4); print $4 }'`
 if [ "$JVMINST" = "1060" ]; then
     echo Service $1 not installed on server
     sc queryex $1
-    exit 1060
+    exit $STP_EXIT_CODE_NO_SUCH_SERVICE
 else
     sc start $1 > /dev/null
     if [ $? -ne 0 ]; then
