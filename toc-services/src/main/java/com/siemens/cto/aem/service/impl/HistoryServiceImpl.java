0685f3b8ce27f92922e48d3ce9152d1058886895
JC043760 <jedd.cuison@cerner.com>
2015-12-07 11:43:05 -0500

    Get rid of the hard coded initial number of record to display for the history and instead retrieve the said value from the property file.

--- toc-services/src/main/java/com/siemens/cto/aem/service/impl/HistoryServiceImpl.java
+++ toc-services/src/main/java/com/siemens/cto/aem/service/impl/HistoryServiceImpl.java
@@ -1,9 +1,11 @@
 package com.siemens.cto.aem.service.impl;
 
+import com.siemens.cto.aem.common.properties.ApplicationProperties;
 import com.siemens.cto.aem.persistence.dao.HistoryDao;
 import com.siemens.cto.aem.persistence.jpa.domain.JpaGroup;
 import com.siemens.cto.aem.persistence.jpa.domain.JpaHistory;
 import com.siemens.cto.aem.service.HistoryService;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +18,8 @@ import java.util.List;
  */
 public class HistoryServiceImpl implements HistoryService {
 
+    private static final long DEFAULT_NUM_OF_REC = 30;
+    public static final String HISTORY_INITIAL_LOAD_NUM_OF_REC = "history.initial-load-num-of-rec";
     private final HistoryDao historyDao;
 
     @Autowired
@@ -35,7 +39,8 @@ public class HistoryServiceImpl implements HistoryService {
 
     @Override
     public List<JpaHistory> read(String groupName) {
-        return historyDao.read(groupName, 30); // TODO: Number of records should be in a property file.
+        return historyDao.read(groupName, NumberUtils.toLong(ApplicationProperties.get(HISTORY_INITIAL_LOAD_NUM_OF_REC),
+                                                             DEFAULT_NUM_OF_REC));
     }
 
 }
