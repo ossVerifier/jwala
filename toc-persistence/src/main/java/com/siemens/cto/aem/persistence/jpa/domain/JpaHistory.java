2c9268136f490a6447003e31baf8228fcecd83c9
JC043760 <jedd.cuison@cerner.com>
2015-12-15 11:27:00 -0500

    Make getResultList return/map values to the entity class instead of just object.

--- toc-persistence/src/main/java/com/siemens/cto/aem/persistence/jpa/domain/JpaHistory.java
+++ toc-persistence/src/main/java/com/siemens/cto/aem/persistence/jpa/domain/JpaHistory.java
@@ -9,11 +9,9 @@ import javax.persistence.*;
 @Table(name = "history", uniqueConstraints = {@UniqueConstraint(columnNames = {"id"})})
 @NamedQueries({
     @NamedQuery(name = JpaHistory.QRY_GET_HISTORY_BY_GROUP_NAME,
-                query = "SELECT h.serverName, h.createBy, h.createDate, h.event, h.eventTypeValue FROM JpaHistory h " +
-                        "WHERE h.group.name = :groupName"),
+                query = "SELECT h FROM JpaHistory h WHERE h.group.name = :groupName"),
     @NamedQuery(name = JpaHistory.QRY_GET_HISTORY_BY_GROUP_NAME_AND_SERVER_NAME,
-                query = "SELECT h.serverName, h.createBy, h.createDate, h.event, h.eventTypeValue FROM JpaHistory h " +
-                        "WHERE h.group.name = :groupName AND h.serverName = :serverName")
+                query = "SELECT h FROM JpaHistory h WHERE h.group.name = :groupName AND h.serverName = :serverName")
 })
 public class JpaHistory extends AbstractEntity<JpaHistory, History> {
 
