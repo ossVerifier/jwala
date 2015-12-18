d48184137f3842d4e015c36c0527687088833d42
JC043760 <jedd.cuison@cerner.com>
2015-12-18 17:43:46 -0500

    Add test for create and update web server.

--- toc-persistence/src/test/java/com/siemens/cto/aem/persistence/jpa/service/WebServerCrudServiceImplTest.java
+++ toc-persistence/src/test/java/com/siemens/cto/aem/persistence/jpa/service/WebServerCrudServiceImplTest.java
@@ -1,6 +1,10 @@
 package com.siemens.cto.aem.persistence.jpa.service;
 
 import com.siemens.cto.aem.common.configuration.TestExecutionProfile;
+import com.siemens.cto.aem.common.domain.model.group.Group;
+import com.siemens.cto.aem.common.domain.model.path.FileSystemPath;
+import com.siemens.cto.aem.common.domain.model.path.Path;
+import com.siemens.cto.aem.common.domain.model.webserver.WebServer;
 import com.siemens.cto.aem.persistence.configuration.TestJpaConfiguration;
 import com.siemens.cto.aem.persistence.jpa.service.impl.WebServerCrudServiceImpl;
 import org.junit.Test;
@@ -16,7 +20,15 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
+ * Integration test for {@link WebServerCrudServiceImpl}
+ *
  * Created by JC043760 on 12/16/2015.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -31,13 +43,34 @@ public class WebServerCrudServiceImplTest {
     private WebServerCrudServiceImpl impl;
 
     @Test
-    public void testCreateWebServer() {
+    public void testCreateAndUpdateWebServer() {
+        final WebServer newWebServer = new WebServer(null,
+                                                     new ArrayList<Group>(),
+                                                     "zWebServer",
+                                                     "zHost",
+                                                     8080,
+                                                     443,
+                                                     new Path("any"),
+                                                     new FileSystemPath("any"),
+                                                     new Path("any"),
+                                                     new Path("any"));
+        final WebServer createdWebServer = impl.createWebServer(newWebServer, "me");
+        assertTrue(createdWebServer.getId() != null);
+        assertTrue(createdWebServer.getId().getId() != null);
+        assertEquals(newWebServer.getName(), createdWebServer.getName());
 
-    }
-
-    @Test
-    public void testUpdateWebServer() {
-
+        final WebServer editedWebServer = new WebServer(createdWebServer.getId(),
+                                                        new ArrayList<Group>(),
+                                                        "zWebServerx",
+                                                        "zHostx",
+                                                        808,
+                                                        44,
+                                                        new Path("anyx"),
+                                                        new FileSystemPath("anyx"),
+                                                        new Path("anyx"),
+                                                        new Path("anyx"));
+        final WebServer updatedWebServer = impl.updateWebServer(editedWebServer, "me");
+        assertEquals(editedWebServer.getId().getId(), updatedWebServer.getId().getId());
     }
 
     @Configuration
