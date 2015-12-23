21a3aa11ec85880c7e88618a73eac7904377cb3f
JC043760 <jedd.cuison@cerner.com>
2015-12-23 18:00:58 -0500

    Do not show status when JVM stateString is FAILED, START_SENT or STOP_SENT on initial rendering of the status widget.

--- toc-webapp/src/main/webapp/resources/js/react/group-operations/GroupOperationsDataTable.js
+++ toc-webapp/src/main/webapp/resources/js/react/group-operations/GroupOperationsDataTable.js
@@ -312,9 +312,14 @@ var GroupOperationsDataTable = React.createClass({
                                                         if (self.props.parent.pollError) {
                                                             statusWidget.setStatus(GroupOperations.UNKNOWN_STATE,  new Date(), "");
                                                         } else {
-                                                            statusWidget.setStatus(data.applicationResponseContent[0].stateString,
-                                                                                   data.applicationResponseContent[0].asOf,
-                                                                                   data.applicationResponseContent[0].message);
+                                                            if (data.applicationResponseContent[0].stateString !== GroupOperations.FAILED &&
+                                                                data.applicationResponseContent[0].stateString !== GroupOperations.START_SENT &&
+                                                                data.applicationResponseContent[0].stateString !== GroupOperations.STOP_SENT) {
+                                                                    statusWidget.setStatus(data.applicationResponseContent[0].stateString,
+                                                                                           data.applicationResponseContent[0].asOf,
+                                                                                           data.applicationResponseContent[0].message);
+
+                                                            }
                                                         }
                                                     });
                     });
