a3e8fff451888c98b4eea323008fa1422a7d96ea
jm036438 <gitcentric@siemens.com>
2016-01-19 21:49:22 -0500

    add unit tests for sonar @GitCentric Sync - AccuRev promote transaction #152932

--- toc-services/src/test/java/com/siemens/cto/aem/service/webserver/impl/WebServerControlServiceImplVerifyTest.java
+++ toc-services/src/test/java/com/siemens/cto/aem/service/webserver/impl/WebServerControlServiceImplVerifyTest.java
@@ -1,5 +1,7 @@
 package com.siemens.cto.aem.service.webserver.impl;
 
+import com.siemens.cto.aem.common.domain.model.fault.AemFaultType;
+import com.siemens.cto.aem.common.domain.model.group.Group;
 import com.siemens.cto.aem.common.domain.model.id.Identifier;
 import com.siemens.cto.aem.common.domain.model.state.CurrentState;
 import com.siemens.cto.aem.common.domain.model.state.StateType;
@@ -7,10 +9,16 @@ import com.siemens.cto.aem.common.domain.model.user.User;
 import com.siemens.cto.aem.common.domain.model.webserver.WebServer;
 import com.siemens.cto.aem.common.domain.model.webserver.WebServerControlOperation;
 import com.siemens.cto.aem.common.domain.model.webserver.WebServerReachableState;
+import com.siemens.cto.aem.common.exception.InternalErrorException;
+import com.siemens.cto.aem.common.exec.CommandOutput;
+import com.siemens.cto.aem.common.exec.ExecCommand;
+import com.siemens.cto.aem.common.exec.ExecReturnCode;
 import com.siemens.cto.aem.common.request.state.SetStateRequest;
 import com.siemens.cto.aem.common.request.webserver.ControlWebServerRequest;
+import com.siemens.cto.aem.control.command.PlatformCommandProvider;
 import com.siemens.cto.aem.control.command.RemoteCommandExecutor;
 import com.siemens.cto.aem.control.webserver.command.impl.WindowsWebServerPlatformCommandProvider;
+import com.siemens.cto.aem.exception.CommandFailureException;
 import com.siemens.cto.aem.persistence.jpa.domain.JpaWebServer;
 import com.siemens.cto.aem.persistence.jpa.type.EventType;
 import com.siemens.cto.aem.service.HistoryService;
@@ -30,9 +38,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 
 import java.net.URI;
+import java.util.HashSet;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
 
@@ -111,5 +121,57 @@ public class WebServerControlServiceImplVerifyTest extends VerificationBehaviorS
                 actualSetStateCommand.getNewState().getState());
 
         verify(mockHistoryService).createHistory(anyString(), anyList(), anyString(), any(EventType.class), anyString());
+    }
+
+    @Test
+    public void testStart() throws CommandFailureException {
+        final Identifier<WebServer> webServerIdentifier = new Identifier<>(12L);
+        WebServer webserver = new WebServer(webServerIdentifier, new HashSet<Group>(), "testWebServer");
+        when(webServerService.getWebServer(any(Identifier.class))).thenReturn(webserver);
+        ExecReturnCode execReturnCode = new ExecReturnCode(0);
+        CommandOutput commandOutput = new CommandOutput(execReturnCode, "SUCCESS", "");
+        when(commandExecutor.executeRemoteCommand(anyString(),anyString(),any(WebServerControlOperation.class),any(PlatformCommandProvider.class))).thenReturn(commandOutput);
+        ControlWebServerRequest controlWSRequest = new ControlWebServerRequest(webServerIdentifier, WebServerControlOperation.START);
+        webServerControlService.controlWebServer(controlWSRequest, user);
+
+        CommandOutput commandOutputProcessKilled = new CommandOutput(new ExecReturnCode(255), "", "PROCESS KILLED");
+        when(commandExecutor.executeRemoteCommand(anyString(),anyString(),any(WebServerControlOperation.class),any(PlatformCommandProvider.class))).thenReturn(commandOutputProcessKilled);
+        CommandOutput returnOutput = webServerControlService.controlWebServer(controlWSRequest, user);
+        assertEquals("FORCED STOPPED", returnOutput.getStandardOutput());
+
+        CommandOutput commandOutputProcessFailed = new CommandOutput(new ExecReturnCode(1), "", "TEST FAILED");
+        when(commandExecutor.executeRemoteCommand(anyString(),anyString(),any(WebServerControlOperation.class),any(PlatformCommandProvider.class))).thenReturn(commandOutputProcessFailed);
+        webServerControlService.controlWebServer(controlWSRequest, user);
+
+        when(commandExecutor.executeRemoteCommand(anyString(), anyString(), any(WebServerControlOperation.class), any(PlatformCommandProvider.class))).thenThrow(new CommandFailureException(new ExecCommand("Failed exec"), new Throwable("Failed remote command")));
+        try {
+            webServerControlService.controlWebServer(controlWSRequest, user);
+        } catch (InternalErrorException iee){
+            assertTrue(iee.getMessageResponseStatus().equals(AemFaultType.REMOTE_COMMAND_FAILURE));
+        }
+    }
+
+    @Test
+    public void testSecureCopy() throws CommandFailureException {
+        final Identifier<WebServer> webServerIdentifier = new Identifier<>(12L);
+        WebServer webserver = new WebServer(webServerIdentifier, new HashSet<Group>(), "testWebServer");
+        when(webServerService.getWebServer(anyString())).thenReturn(webserver);
+        when(webServerStateService.getCurrentState(any(Identifier.class))).thenReturn(new CurrentState(webServerIdentifier, WebServerReachableState.WS_REACHABLE, DateTime.now(), StateType.WEB_SERVER));
+        CommandOutput returnOutput = webServerControlService.secureCopyHttpdConf("testWebServer", "./source", "./dest");
+        assertEquals(new ExecReturnCode(1), returnOutput.getReturnCode());
+
+        when(webServerStateService.getCurrentState(any(Identifier.class))).thenReturn(new CurrentState(webServerIdentifier, WebServerReachableState.WS_UNREACHABLE, DateTime.now(), StateType.WEB_SERVER));
+        CommandOutput successReturnOutput = new CommandOutput(new ExecReturnCode(0), "SUCCESSS", "");
+        when(commandExecutor.executeRemoteCommand(anyString(), anyString(), any(WebServerControlOperation.class), any(PlatformCommandProvider.class), anyString(), anyString())).thenReturn(successReturnOutput);
+        returnOutput = webServerControlService.secureCopyHttpdConf("testWebServer", "./source", "./dest");
+        assertEquals(new ExecReturnCode(0), returnOutput.getReturnCode());
+
+        CommandOutput failedReturnOutput = new CommandOutput(new ExecReturnCode(1), "FAILED", "");
+        when(commandExecutor.executeRemoteCommand(anyString(), anyString(), any(WebServerControlOperation.class), any(PlatformCommandProvider.class), anyString(), anyString())).thenReturn(failedReturnOutput);
+        try {
+            webServerControlService.secureCopyHttpdConf("testWebServer", "./source", "./dest");
+        } catch(InternalErrorException ie) {
+            assertEquals(AemFaultType.REMOTE_COMMAND_FAILURE, ie.getMessageResponseStatus());
+        }
     }
 }
