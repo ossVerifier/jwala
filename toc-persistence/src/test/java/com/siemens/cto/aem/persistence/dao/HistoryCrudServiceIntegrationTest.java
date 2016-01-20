cb170ee1099b5511734b32d02f1d1311866f0451
JC043760 <jedd.cuison@cerner.com>
2016-01-20 00:45:48 -0500

    Complete the test cases.

--- toc-persistence/src/test/java/com/siemens/cto/aem/persistence/dao/HistoryCrudServiceIntegrationTest.java
+++ toc-persistence/src/test/java/com/siemens/cto/aem/persistence/dao/HistoryCrudServiceIntegrationTest.java
@@ -2,10 +2,16 @@ package com.siemens.cto.aem.persistence.dao;
 
 import com.siemens.cto.aem.common.configuration.TestExecutionProfile;
 import com.siemens.cto.aem.common.domain.model.group.Group;
+import com.siemens.cto.aem.common.domain.model.id.Identifier;
 import com.siemens.cto.aem.persistence.configuration.TestJpaConfiguration;
+import com.siemens.cto.aem.persistence.jpa.domain.JpaGroup;
+import com.siemens.cto.aem.persistence.jpa.domain.JpaHistory;
+import com.siemens.cto.aem.persistence.jpa.service.GroupCrudService;
+import com.siemens.cto.aem.persistence.jpa.service.impl.GroupCrudServiceImpl;
 import com.siemens.cto.aem.persistence.jpa.service.impl.HistoryCrudServiceImpl;
 import com.siemens.cto.aem.persistence.jpa.service.HistoryCrudService;
 import com.siemens.cto.aem.persistence.jpa.type.EventType;
+import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +23,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Integration test for {@link HistoryCrudService}.
@@ -35,18 +45,40 @@ public class HistoryCrudServiceIntegrationTest {
     static class Config {
 
         @Bean
-        HistoryCrudService getHistoryDao() {
+        GroupCrudService getGroupCrudService() {
+            return new GroupCrudServiceImpl();
+        }
+
+        @Bean
+        HistoryCrudService getHistoryCrudService() {
             return new HistoryCrudServiceImpl();
         }
 
     }
 
     @Autowired
+    private GroupCrudService groupCrudService;
+
+    @Autowired
     private HistoryCrudService historyCrudService;
 
     @Test
-    public void testWriteHistory() {
-        historyCrudService.createHistory("any", null, "Testing...", EventType.USER_ACTION, "any");
+    public void testWriteAndReadHistory() {
+        final String SERVER_NAME = "zServerName";
+        final JpaGroup jpaGroup = new JpaGroup();
+        jpaGroup.setName("zGroup");
+        groupCrudService.create(jpaGroup);
+        historyCrudService.createHistory(SERVER_NAME, new Group(new Identifier<Group>(jpaGroup.getId()), "zGroup"), "Testing...", EventType.USER_ACTION, "any");
+
+        List<JpaHistory> jpaHistoryList = historyCrudService.findHistory(jpaGroup.getName(), SERVER_NAME, 1);
+        assertEquals(1, jpaHistoryList.size());
+        assertEquals(EventType.USER_ACTION, jpaHistoryList.get(0).getEventType());
+
+        jpaHistoryList = historyCrudService.findHistory(jpaGroup.getName(), SERVER_NAME, null);
+        assertEquals(1, jpaHistoryList.size());
+
+        jpaHistoryList = historyCrudService.findHistory(jpaGroup.getName(), null, null);
+        assertEquals(1, jpaHistoryList.size());
     }
 
 }
