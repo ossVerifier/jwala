02b03a1b726af243a1936a5e3cb14a2eaed6f742
JC043760 <jedd.cuison@cerner.com>
2016-03-18 14:14:40 -0400

    1. Add logging. 2. Address commit warnings.

--- toc-services/src/main/java/com/siemens/cto/aem/service/group/impl/spring/component/GroupStateNotificationServiceImpl.java
+++ toc-services/src/main/java/com/siemens/cto/aem/service/group/impl/spring/component/GroupStateNotificationServiceImpl.java
@@ -15,6 +15,8 @@ import com.siemens.cto.aem.persistence.jpa.service.WebServerCrudService;
 import com.siemens.cto.aem.service.exception.GroupStateNotificationServiceException;
 import com.siemens.cto.aem.service.group.GroupStateNotificationService;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Async;
@@ -29,6 +31,8 @@ import java.util.List;
  */
 @Service
 public class GroupStateNotificationServiceImpl implements GroupStateNotificationService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GroupStateNotificationServiceImpl.class);
 
     private static final String NULL_STR = "NULL_STR";
 
@@ -45,8 +49,11 @@ public class GroupStateNotificationServiceImpl implements GroupStateNotification
 
     @Override
     @Async
+    @SuppressWarnings("unchecked")
     public void retrieveStateAndSendToATopic(final Identifier id, final Class aClass, final String topic) {
+        LOGGER.debug("Synchronizing on {} and {}...", id, aClass);
         synchronized ((aClass.getSimpleName() + id.toString()).intern()) {
+            LOGGER.debug("Thread locked on {} and {}...!", id, aClass);
             final List<JpaGroup> groups;
             if (Jvm.class.getName().equals(aClass.getName())) {
                 final JpaJvm jvm = jvmCrudService.getJvm(id);
@@ -55,10 +62,10 @@ public class GroupStateNotificationServiceImpl implements GroupStateNotification
                 final JpaWebServer webServer = webServerCrudService.getWebServerAndItsGroups(id.getId());
                 groups = webServer.getGroups();
             } else {
-                throw new GroupStateNotificationServiceException("Invalid class parameter: " +
-                        (aClass == null ? NULL_STR : aClass.getName()) + "!");
+                final String errMsg = "Invalid class parameter: " + aClass.getName() + "!";
+                LOGGER.error(errMsg);
+                throw new GroupStateNotificationServiceException(errMsg);
             }
-
 
             for (final JpaGroup group: groups) {
                 final Long jvmStartedCount = jvmCrudService.getJvmStartedCount(group.getName());
@@ -69,8 +76,10 @@ public class GroupStateNotificationServiceImpl implements GroupStateNotification
                         GroupState.GRP_UNKNOWN, DateTime.now(), StateType.GROUP, webServerCount, webServerStartedCount,
                         jvmCount, jvmStartedCount);
                 simpMessagingTemplate.convertAndSend(TOPIC_SERVER_STATES, groupState);
+                LOGGER.debug("Group '{}' state = {}", group.getName(), groupState);
             }
         }
+        LOGGER.debug("Thread locked on {} and {} released!", id, aClass);
     }
 
 }
