7df4e5af3b66bb409a5157c8c593ced871475dd7
JC043760 <jedd.cuison@cerner.com>
2016-05-17 17:45:21 -0400

    1. Set the parent of the selected entity. 2. Remove deprecated render function.

--- toc-webapp/src/main/webapp/resources/js/react/ResourceAttrPane.js
+++ toc-webapp/src/main/webapp/resources/js/react/ResourceAttrPane.js
@@ -35,129 +35,44 @@ var ResourceAttrPane = React.createClass({
                 alert(e);
             });
     },
-
-    // TODO: Remove when new code that replaces this is complete.
-    render_: function() {
-        if (this.state.attributes === null ) {
-            return <div className="attr-list ui-widget-content" style={{padding: "2px 2px"}}><span>Please select a JVM, Web Server or Web Application...</span></div>;
-        }
-
-        var entity = this.state.attributes.rtreeListMetaData.entity ;
-
-        // By design the entity name ends with 's' as in JVMs, webServers or webApps.
-        // We need to prefix the attributes with the entity name e.g. jvm.name so we need to strip the 's'.
-        entity = entity.substring(0, entity.length - 1);
-
-        var reactAttributeElements = [];
-        for (attr in this.state.attributes) {
-
-            var children;
-            if (attr === "rtreeListMetaData") {
-                var entityType = this.state.attributes[attr].entity;
-                if ( entityType === "webServers" || entityType === "jvms" || entityType === "webServerSection" || entityType === "jvmSection") {
-                    var wsArray = [];
-                    var jvmArray = [];
-                    var webApps = {};
-
-                    // Web Servers
-                    if (this.state.attributes[attr].parent.webServers !== undefined) {
-                        for (var wsIdx = 0; wsIdx < this.state.attributes[attr].parent.webServers.length; wsIdx++) {
-                            var webServer = this.state.attributes[attr].parent.webServers[wsIdx];
-                            wsArray.push(webServer);
-                        }
-                    }
-
-                    // JVMs
-                    if (this.state.attributes[attr].parent.jvms !== undefined) {
-                        for (var jvmIdx = 0; jvmIdx < this.state.attributes[attr].parent.jvms.length; jvmIdx++) {
-                            var jvm = this.state.attributes[attr].parent.jvms[jvmIdx];
-
-                            jvmArray.push(jvm);
-
-                            // Web Applications
-                            if (jvm.webApps !== undefined && jvm.webApps.length > 0) {
-                                jvm.webApps.forEach(function(webApp) {
-                                    webApps[webApp.name] = webApp;
-                                });
-                            }
-                        }
-                    }
-
-                    if (wsArray.length > 0) {
-                        reactAttributeElements.push(<tr><td colSpan="2"><WebServerTable attributes={wsArray}/></td></tr>);
-                    }
-
-                    if (jvmArray.length > 0) {
-                        reactAttributeElements.push(<tr><td colSpan="2"><JvmTable attributes={jvmArray}/></td></tr>);
-                    }
-
-                    if ((entityType === "webServers" || entityType === "webServerSection" || entityType === "jvmSection") && Object.keys(webApps).length > 0) {
-                        reactAttributeElements.push(<tr><td colSpan="2"><WebAppTable attributes={webApps} /></td></tr>);
-                    }
-                } else if (entityType === "webApps") {
-                    console.log(this.state.attributes[attr]);
-                    console.log(this.state.attributes[attr].parent);
-
-
-                    for (parentAttr in this.state.attributes[attr].parent) {
-                        if (typeof(this.state.attributes[attr].parent[parentAttr]) !== "object") {
-                            reactAttributeElements.push(<tr><td>{"${webApp.jvm." + parentAttr + "}"}</td>
-                                                            <td>{this.state.attributes[attr].parent[parentAttr]}</td></tr>);
-                        }
-                    }
-
-
-
-                }
-            }
-
-            if (typeof(this.state.attributes[attr]) !== "object" && entity !== "webServerSectio" && entity !== "jvmSectio") {
-                reactAttributeElements.push(React.createElement(Attribute,
-                                                    {entity: entity, key: attr, property: attr, value: this.state.attributes[attr]}));
-            }
-        }
-
-        return <div className="attr-list ui-widget-content">
-                    <table className="attr-table">
-                        <thead>
-                            <th>Property</th><th>Value</th>
-                        </thead>
-                        <tbody>{reactAttributeElements}</tbody>
-                    </table>
-               </div>;
-    },
-    setCurrentlySelectedEntityData: function(data, entityName) {
+    setCurrentlySelectedEntityData: function(data, entityName, parent) {
         if (this.state.attributes) {
             var newAttributes = {};
             for (key in this.state.attributes) {
                 newAttributes[key] = this.state.attributes[key];
             }
 
-            var newData = {};
-            for (key in data) {
-                if (key !== "rtreeListMetaData") {
-                    newData[key] = data[key];
-                }
-            }
-
+            // Assign parent data
             var newEntityName;
             switch (entityName) {
                 case "jvmSection":
                     newAttributes["jvms"] = newData.jvms;
+                    newAttributes["jvms"].forEach(function(item) {
+                        item["parentGroup"] = newParentData;
+                    });
                     break;
                 case "webServerSection":
                     newAttributes["webServers"] = newData.webServers;
+                    newAttributes["webServers"].forEach(function(item) {
+                        item["parentGroup"] = newParentData;
+                    });
                     break;
                 case "webAppSection":
                     newAttributes["webApps"] = newData.webApps;
+                    newAttributes["webApps"].forEach(function(item) {
+                        item["parentGroup"] = newParentData;
+                    });
                     break;
                 case "webApps":
+                     newData["parentJvm"] = newParentData;
                      newAttributes["webApp"] = newData;
                     break;
                 case "jvms":
+                     newData["parentGroup"] = newParentData;
                      newAttributes["jvm"] = newData;
                     break;
                 case "webServers":
+                     newData["parentGroup"] = newParentData;
                      newAttributes["webServer"] = newData;
                     break;
             }
