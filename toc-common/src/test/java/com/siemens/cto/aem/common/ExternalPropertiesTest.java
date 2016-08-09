1aa9ff60a75c5b090c5be7685fa8ff2229a48f5d
jeffrey <jeffreymanhood@gmail.com>
2016-08-09 15:24:49 -0400

    for the external properties test make a copy of the test file that gets modified in the build directory so it doesn't get flagged in git

--- toc-common/src/test/java/com/siemens/cto/aem/common/ExternalPropertiesTest.java
+++ toc-common/src/test/java/com/siemens/cto/aem/common/ExternalPropertiesTest.java
@@ -1,8 +1,10 @@
 package com.siemens.cto.aem.common;
 
 import com.siemens.cto.aem.common.exception.ApplicationException;
+import com.siemens.cto.aem.common.properties.ApplicationProperties;
 import com.siemens.cto.aem.common.properties.ExternalProperties;
 import junit.framework.TestCase;
+import org.springframework.util.FileCopyUtils;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -12,6 +14,7 @@ import java.nio.file.StandardOpenOption;
 public class ExternalPropertiesTest extends TestCase {
 
     public static final String SRC_TEST_RESOURCES_PROPERTIES = new File(".").getAbsolutePath() + "/src/test/resources/properties/";
+    public static final String BUILD_TEST_RESOURCES_PROPERTIES = new File(".").getAbsolutePath() + "/build/";
     public static final String EXTERNAL_PROPERTIES = "external.properties";
 
     public void testBadPropertiesPath() {
@@ -41,11 +44,13 @@ public class ExternalPropertiesTest extends TestCase {
         assertEquals(Boolean.TRUE, ExternalProperties.getAsBoolean("boolean.property"));
     }
 
-/*    public void testReload() throws IOException {
-        ExternalProperties.setPropertiesFilePath(SRC_TEST_RESOURCES_PROPERTIES + EXTERNAL_PROPERTIES);
+    public void testReload() throws IOException {
+        FileCopyUtils.copy(new File(SRC_TEST_RESOURCES_PROPERTIES + EXTERNAL_PROPERTIES), new File(BUILD_TEST_RESOURCES_PROPERTIES + EXTERNAL_PROPERTIES));
+
+        ExternalProperties.setPropertiesFilePath(BUILD_TEST_RESOURCES_PROPERTIES + EXTERNAL_PROPERTIES);
 
         final String propertyToAdd = "test.reload=true";
-        writeNewPropertyToFile(propertyToAdd, SRC_TEST_RESOURCES_PROPERTIES + EXTERNAL_PROPERTIES);
+        writeNewPropertyToFile(propertyToAdd, BUILD_TEST_RESOURCES_PROPERTIES + EXTERNAL_PROPERTIES);
 
         ExternalProperties.reload();
         assertEquals("string property", ExternalProperties.get("string.property"));
@@ -53,40 +58,9 @@ public class ExternalPropertiesTest extends TestCase {
         assertEquals(Boolean.TRUE, ExternalProperties.getAsBoolean("boolean.property"));
         assertEquals(Boolean.TRUE, ExternalProperties.getAsBoolean("test.reload"));
         assertNull(ApplicationProperties.get("home team"));
-
-        deleteLineFromFile(propertyToAdd, SRC_TEST_RESOURCES_PROPERTIES + EXTERNAL_PROPERTIES);
-    }*/
+    }
 
     private void writeNewPropertyToFile(String propertyToAdd, String propertiesFilePath) throws IOException {
         Files.write(Paths.get(propertiesFilePath), propertyToAdd.getBytes(), StandardOpenOption.APPEND);
-    }
-
-    private void deleteLineFromFile(String lineToDelete, String propertiesFilePath) throws IOException {
-        File inputFile = new File(propertiesFilePath);
-
-        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-
-        String lineToRemove;
-        lineToRemove = lineToDelete;
-        String currentLine = reader.readLine();
-        StringBuffer fileContent = new StringBuffer();
-
-        while (currentLine != null) {
-            // trim newline when comparing with lineToRemove
-            String trimmedLine = currentLine.trim();
-            if (!trimmedLine.equals(lineToRemove)) {
-                fileContent.append(currentLine);
-            }
-
-            currentLine = reader.readLine();
-            if (currentLine != null) {
-                fileContent.append(System.getProperty("line.separator"));
-            }
-        }
-        reader.close();
-
-        try (PrintWriter printWriter = new PrintWriter(inputFile)) {
-            printWriter.print(fileContent);
-        }
     }
 }
