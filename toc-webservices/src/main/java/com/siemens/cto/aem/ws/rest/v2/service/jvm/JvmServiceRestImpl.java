d0e0d26802573c6db2e5e6d5b48d96bf5c5aec26
JC043760 <jedd.cuison@cerner.com>
2016-08-11 18:30:22 -0400

    Define the jvm rest services

---
+++ toc-webservices/src/main/java/com/siemens/cto/aem/ws/rest/v2/service/jvm/JvmServiceRestImpl.java
@@ -0,0 +1,52 @@
+package com.siemens.cto.aem.ws.rest.v2.service.jvm;
+
+import com.siemens.cto.aem.service.jvm.JvmService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+
+/**
+ * Implements {JvmServiceRest}
+ *
+ * Created by JC043760 on 8/9/2016.
+ */
+public class JvmServiceRestImpl implements JvmServiceRest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JvmServiceRestImpl.class);
+
+    private final JvmService jvmService;
+
+    public JvmServiceRestImpl(final JvmService jvmService) {
+        this.jvmService = jvmService;
+    }
+
+    @Override
+    public Response getJvm(final String name) {
+        LOGGER.debug("Get JVM requested: {}", name);
+        // TODO: Should we do this ? toDecrypted ?
+        // Note: I don't like the current ResponseBuilder specially when returning errors e.g. AEM_xx
+        //       Can we think about this in the new future ?
+        return Response.ok(jvmService.getJvm(name).toDecrypted()).build();
+    }
+
+    @Override
+    public Response createJvm(final JvmRequestData jvmRequestData) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Response updateJvm(final JvmRequestData jvmRequestData) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Response removeJvm(final String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Response controlJvm(final String name, final JvmControlDataRequest jvmControlDataRequest) {
+        throw new UnsupportedOperationException();
+    }
+}
