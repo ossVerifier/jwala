31fbc1066db3a748daf2ca1a49d87c9c2d4e5cb8
JC043760 <jedd.cuison@cerner.com>
2016-08-25 18:39:33 -0400

    Implement property file value retrieval via spring context placeholder-property

--- jwala-h2-lifecyclelistener/src/main/java/com/cerner/jwala/service/impl/H2Service.java
+++ jwala-h2-lifecyclelistener/src/main/java/com/cerner/jwala/service/impl/H2Service.java
@@ -3,6 +3,7 @@ package com.cerner.jwala.service.impl;
 import com.cerner.jwala.service.DbService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * Implements {@link DbService}
@@ -13,13 +14,16 @@ public class H2Service implements DbService {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(H2Service.class);
 
+    @Value("${h2.tcpPort}")
+    private String tcpPort;
+
     public H2Service() {
         LOGGER.info("H2 service initialized");
     }
 
     @Override
     public void startServer() {
-        LOGGER.info("Starting H2...");
+        LOGGER.info("Starting H2 - tcpPort = {}", tcpPort);
     }
 
     @Override
