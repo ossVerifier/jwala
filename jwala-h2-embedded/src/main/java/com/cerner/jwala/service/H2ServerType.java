86757d366c98468455d6b3dca4341f8b3d1bc66b
JC043760 <jedd.cuison@cerner.com>
2016-08-30 18:29:49 -0400

    Remove duplicate "Created by"

--- jwala-h2-embedded/src/main/java/com/cerner/jwala/service/H2ServerType.java
+++ jwala-h2-embedded/src/main/java/com/cerner/jwala/service/H2ServerType.java
@@ -4,7 +4,6 @@ package com.cerner.jwala.service;
  * Enum listing types of H2 server
  *
  * Created by JC043760 on 8/30/2016
- * Created by JC043760 on 8/30/2016
  */
 public enum H2ServerType {
     TCP, WEB, PG
