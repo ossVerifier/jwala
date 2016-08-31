d3cd7f7b7a408ccb6fa1948f7dfc637cb69a2785
JC043760 <jedd.cuison@cerner.com>
2016-08-31 12:39:45 -0400

    Support legacy JGroups reporting message

--- jwala-services/src/main/java/com/cerner/jwala/service/jvm/state/JvmStateReceiverAdapter.java
+++ jwala-services/src/main/java/com/cerner/jwala/service/jvm/state/JvmStateReceiverAdapter.java
@@ -5,6 +5,7 @@ import com.cerner.jwala.common.domain.model.jvm.Jvm;
 import com.cerner.jwala.common.domain.model.jvm.JvmState;
 import com.cerner.jwala.service.jvm.JvmStateService;
 import org.apache.catalina.LifecycleState;
+import org.apache.commons.lang.IllegalClassException;
 import org.apache.commons.lang3.StringUtils;
 import org.jgroups.Address;
 import org.jgroups.Message;
@@ -55,11 +56,57 @@ public class JvmStateReceiverAdapter extends ReceiverAdapter {
     public void receive(Message jgroupMessage) {
         final Address src = jgroupMessage.getSrc();
         final Map<String, Object> serverInfoMap = (Map) jgroupMessage.getObject();
+        final JvmState jvmState;
+        final String jvmId;
         LOGGER.debug("Received JGroups JVM state message {} {}", src, serverInfoMap);
-        final LifecycleState lifecycleState = (LifecycleState) serverInfoMap.get(STATE_KEY);
-        final JvmState jvmState = LIFECYCLE_JWALA_JVM_STATE_REF_MAP.get(lifecycleState);
+
+        LifecycleState lifecycleState = (LifecycleState) serverInfoMap.get(STATE_KEY);
+        if (lifecycleState == null) {
+            // Assume that the message came from the old JGroups listener
+            LOGGER.info("The state key = {} was not found in the lifecycle state map. " +
+                        "Jwala will proceed with the assumption that that the message came from the legacy " +
+                        "JGroups reporting lifecycle listener.", STATE_KEY);
+
+            Object legacyIdKey = null;
+            Object legacyStateKey = null;
+            try {
+                // If this if from legacy JGroups reporting lifecycle listener, the key will be of type ReportingJmsMessageKey
+                // found in the infrastructure provided jar which is provided by the container therefore we just iterate
+                // trough the keys to try to look for the "STATE" string. Note: We don't want to use ReportingJmsMessageKey
+                // to make this compatible to containers that do not have infrastructure provided jar hence the key
+                // iteration.
+                for (Object key: serverInfoMap.keySet()) {
+                    if (key.toString().equalsIgnoreCase(ID_KEY)) {
+                        legacyIdKey = key;
+                    }
+
+                    if (key.toString().equalsIgnoreCase(STATE_KEY)) {
+                        legacyStateKey = key;
+                    }
+
+                    if (legacyIdKey != null && legacyStateKey != null) {
+                        break;
+                    }
+                }
+
+                if (legacyStateKey != null) {
+                    jvmId = serverInfoMap.get(legacyIdKey).toString();
+                    jvmState = JvmState.valueOf(serverInfoMap.get(legacyStateKey).toString());
+                } else {
+                    LOGGER.error("Failed to get the legacy key in {}! Cannot extract the lifecycle from the JGroups message.", serverInfoMap);
+                    return;
+                }
+            } catch (final IllegalArgumentException e) {
+                LOGGER.error("Cannot process legacy key = {} since it is not defined in JvmState enum!", legacyStateKey, e);
+                return;
+            }
+        } else {
+            jvmId = serverInfoMap.get(ID_KEY).toString();
+            jvmState = LIFECYCLE_JWALA_JVM_STATE_REF_MAP.get(lifecycleState);
+        }
+
         if (!JvmState.JVM_STOPPED.equals(jvmState)) {
-            jvmStateService.updateState(new Identifier<Jvm>(serverInfoMap.get(ID_KEY).toString()), jvmState, StringUtils.EMPTY);
+            jvmStateService.updateState(new Identifier<Jvm>(jvmId), jvmState, StringUtils.EMPTY);
         }
     }
 
