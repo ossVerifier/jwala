9641c3a007cc522f23eaec7aedacc0b95b5300f8
Sahil Palvia <Sahil.Palvia@Cerner.com>
2016-09-06 16:20:34 -0400

    SOARCOM-1337 adding zip service class, to enable zipping of a directory

---
+++ jwala-services/src/main/java/com/cerner/jwala/service/zip/impl/ZipDirectoryImpl.java
@@ -0,0 +1,90 @@
+package com.cerner.jwala.service.zip.impl;
+
+import com.cerner.jwala.service.exception.ZipDirectoryException;
+import com.cerner.jwala.service.zip.ZipDirectory;
+import org.apache.openjpa.lib.util.Files;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * Created by SP043299 on 9/6/2016.
+ */
+public class ZipDirectoryImpl implements ZipDirectory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ZipDirectoryImpl.class);
+
+    @Override
+    public void zipDirectory(String source, String destination) {
+        File zipDir = new File(source);
+        if (zipDir.exists()) {
+            if (new File(destination).exists()) {
+                LOGGER.debug("Previous copy of the {} exists, backing it up", destination);
+                String currentDateSuffix = new SimpleDateFormat(".yyyyMMdd_HHmmss").format(new Date());
+                try {
+                    Files.copy(new File(destination), new File(destination + currentDateSuffix));
+                } catch (IOException e) {
+                    LOGGER.error("Error with backing up old zip file", e);
+                    throw new ZipDirectoryException(e);
+                }
+            }
+            LOGGER.debug("Zipping {} to {}", source, destination);
+            List<String> fileNames = new ArrayList<>();
+            getFiles(zipDir, source, fileNames);
+            FileOutputStream fileOutputStream;
+            ZipOutputStream zipOutputStream = null;
+            try {
+                fileOutputStream = new FileOutputStream(destination);
+                zipOutputStream = new ZipOutputStream(fileOutputStream);
+
+                byte[] buffer = new byte[1024];
+                for (String fileName : fileNames) {
+                    LOGGER.debug("Adding {} to {} zipfile", fileName, destination);
+                    ZipEntry zipEntry = new ZipEntry(fileName);
+                    zipOutputStream.putNextEntry(zipEntry);
+
+                    FileInputStream fileInputStream = new FileInputStream(source + "/" + fileName);
+                    int length = 0;
+                    while ((length = fileInputStream.read()) > 0) {
+                        zipOutputStream.write(buffer, 0, length);
+                    }
+                    fileInputStream.close();
+                }
+            } catch (FileNotFoundException e) {
+                LOGGER.error("Error with finding the file ", e);
+                throw new ZipDirectoryException(e);
+            } catch (IOException e) {
+                LOGGER.error("Error with adding next zip entry", e);
+                throw new ZipDirectoryException(e);
+            } finally {
+                if (zipOutputStream != null) {
+                    try {
+                        zipOutputStream.closeEntry();
+                        zipOutputStream.close();
+                    } catch (IOException e) {
+                        LOGGER.error("Error in closing zipoutputstream", e);
+                        throw new ZipDirectoryException(e);
+                    }
+                }
+            }
+        } else {
+            LOGGER.warn("The {} directory does not exist, cannot zip it", source);
+        }
+    }
+
+    private void getFiles(File file, String source, List<String> files) {
+        if (file.isFile()) {
+            files.add(file.getAbsolutePath().substring(source.length() + 1));
+        } else if (file.isDirectory()) {
+            for (String f : file.list()) {
+                getFiles(new File(f), source, files);
+            }
+        }
+    }
+}
