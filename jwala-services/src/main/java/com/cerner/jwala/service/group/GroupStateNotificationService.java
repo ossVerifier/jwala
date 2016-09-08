2824593e90fcce12aac16cf7179cbb1d55520a17
JC043760 <jedd.cuison@cerner.com>
2016-09-08 14:18:47 -0400

    Update java doc

--- jwala-services/src/main/java/com/cerner/jwala/service/group/GroupStateNotificationService.java
+++ jwala-services/src/main/java/com/cerner/jwala/service/group/GroupStateNotificationService.java
@@ -4,7 +4,7 @@ import com.cerner.jwala.common.domain.model.id.Identifier;
 
 /**
  * Retrieve group state details (e.g. running JVM count and Web Sever count) and send the said data to a destination via
- * a messaging system like JMS or Spring STOMP.
+ * a messaging framework like JMS or Spring STOMP.
  *
  * Created by JC043760 on 3/14/2016.
  */
