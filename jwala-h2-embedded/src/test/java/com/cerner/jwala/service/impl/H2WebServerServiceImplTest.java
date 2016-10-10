49e6c8173803c57b920ce8adb14579706417bc6e
jm036438 <jeffrey.mahmood@cerner.com>
2016-10-10 14:02:17 -0400

    add tests for jwala-h2-embedded

---
+++ jwala-h2-embedded/src/test/java/com/cerner/jwala/service/impl/H2WebServerServiceImplTest.java
@@ -0,0 +1,39 @@
+package com.cerner.jwala.service.impl;
+
+import com.cerner.jwala.service.DbServerServiceException;
+import org.h2.tools.Server;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created on 10/10/2016.
+ */
+public class H2WebServerServiceImplTest {
+
+    private H2WebServerServiceImpl service;
+
+    @Before
+    public void setUp() {
+        service = new H2WebServerServiceImpl(null);
+    }
+
+    @Test
+    public void testCreateServer() {
+        String[] params = new String[0];
+        Server result = service.createServer(params);
+        assertEquals("default port", 8082, result.getPort());
+        assertEquals("default status", "Not started", result.getStatus());
+        // default URL is created using the IP address of the running machine so just test for not null
+        assertNotNull("default URL", result.getURL());
+        assertNotNull("default service", result.getService());
+    }
+
+    @Test (expected = DbServerServiceException.class)
+    public void testCreateServerThrowsException() {
+        String[] badParams = new String[]{"-webPort", "ERROR"};
+            service.createServer(badParams);
+    }
+}
