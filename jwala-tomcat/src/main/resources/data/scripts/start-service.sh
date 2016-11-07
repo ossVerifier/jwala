d74f9167448d0013a1c890445ff32b106dc1fcae
Lin-Hung Wu <Lin-Hung.Wu@cerner.com>
2016-11-07 16:16:37 -0500

    SOARCOM-1655 windows service only install with local system account

--- jwala-tomcat/src/main/resources/data/scripts/start-service.sh
+++ jwala-tomcat/src/main/resources/data/scripts/start-service.sh
@@ -11,23 +11,29 @@ if [ "$1" = "" ]; then
     /usr/bin/echo $0 not invoked with service name
     exit $JWALA_EXIT_CODE_NO_OP;
 fi
-
 export JVMINST=`sc queryex $1 | /usr/bin/head -1 | /usr/bin/awk '{ sub(/:/,"",$4); print $4 }'`
 if [ "$JVMINST" = "1060" ]; then
     /usr/bin/echo `sc queryex $1`
     exit $JWALA_EXIT_CODE_NO_SUCH_SERVICE
 else
-    sc start $1 > /dev/null
+    sc start $1 &> output
     if [ $? -ne 0 ]; then
         export SCRESULT=`sc start $1 | /usr/bin/head -1 | /usr/bin/awk '{ sub(/:/,"",$4); print $4 }'`
         if [ "$SCRESULT" = "1056" ]; then
             /usr/bin/echo Service $1 already started.
+            rm output
             exit $JWALA_EXIT_CODE_ABNORMAL_SUCCESS
         fi
-        /usr/bin/echo $1 failed to start and was not already started
+        if [ -s output]; then
+            /usr/bin/echo $1 failed to start and was not already started
+        else
+            /usr/bin/cat output
+        fi
+        rm output
         exit $JWALA_EXIT_CODE_FAILED
     else
         /usr/bin/echo Call to start service $1 was successful
+        rm output
         exit $JWALA_EXIT_CODE_SUCCESS
     fi
 fi
