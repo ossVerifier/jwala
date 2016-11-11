635980df30cbba7b1a003cf77ee469db2450c93d
jm036438 <jeffrey.mahmood@cerner.com>
2016-11-11 09:32:22 -0500

    rename properties files and clean up parent test class

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/util/SeleniumTestCase.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/util/SeleniumTestCase.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -13,28 +14,38 @@ import java.util.concurrent.TimeUnit;
  */
 public class SeleniumTestCase {
 
+    private static final String PROPERTY_JWALA_BASE_URL = "jwala.base.url";
+    private static final String PROPERTY_JWALA_WAIT_BETWEEN_STEPS_TIME_MS = "jwala.wait.between.steps.time.ms";
+    private static final String PROPERTY_JWALA_WAIT_BETWEEN_STEPS = "jwala.wait.between.steps";
+    private static final String PROPERTY_SELENIUM_PROPERTY_FILE = "selenium.property.file";
+    private static final String PROPERTY_WEBDRIVER_NAME = "webdriver.name";
+    private static final String PROPERTY_WEBDRIVER_VALUE = "webdriver.value";
+    private static final String PROPERTY_WEBDRIVER_CLASS = "webdriver.class";
+
+    private static final String JWALA_INTEGRATION_TEST_PROPERTIES = "jwala-integration-test.properties";
+
     protected Properties properties;
     protected WebDriver driver;
     protected String baseUrl;
 
-    public void setUpSeleniumDrivers() throws IOException, InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException, NoSuchMethodException, ClassNotFoundException {
+    public void setUpSeleniumDrivers() throws IOException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, ClassNotFoundException {
         properties = new Properties();
-        final String propertyFile = System.getProperty("selenium.property.file");
+        final String propertyFile = System.getProperty(PROPERTY_SELENIUM_PROPERTY_FILE);
         try (InputStream inputStream = (propertyFile == null || propertyFile.isEmpty()) ?
-                ClassLoader.getSystemResourceAsStream("test.properties") : new FileInputStream(propertyFile)) {
+                ClassLoader.getSystemResourceAsStream(JWALA_INTEGRATION_TEST_PROPERTIES) : new FileInputStream(propertyFile)) {
             properties.load(inputStream);
         }
-        System.setProperty(properties.getProperty("webdriver.name"), properties.getProperty("webdriver.value"));
-        driver = (WebDriver) Class.forName(properties.getProperty("webdriver.class")).getConstructor().newInstance();
-        baseUrl = properties.getProperty("jwala.base.url");
+        System.setProperty(properties.getProperty(PROPERTY_WEBDRIVER_NAME), properties.getProperty(PROPERTY_WEBDRIVER_VALUE));
+        driver = (WebDriver) Class.forName(properties.getProperty(PROPERTY_WEBDRIVER_CLASS)).getConstructor().newInstance();
+        baseUrl = properties.getProperty(PROPERTY_JWALA_BASE_URL);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
 
     public void waitABit() throws IOException, InterruptedException {
-        final Boolean isWaitPropertySet = Boolean.valueOf(properties.getProperty("jwala.wait.between.steps", "false"));
+        final Boolean isWaitPropertySet = Boolean.valueOf(properties.getProperty(PROPERTY_JWALA_WAIT_BETWEEN_STEPS, "false"));
         if (isWaitPropertySet){
-            final long sleepTime = Long.parseLong(properties.getProperty("jwala.wait.between.steps.time.ms", "1000"));
+            final long sleepTime = Long.parseLong(properties.getProperty(PROPERTY_JWALA_WAIT_BETWEEN_STEPS_TIME_MS, "1000"));
             Thread.sleep(sleepTime);
         }
 
