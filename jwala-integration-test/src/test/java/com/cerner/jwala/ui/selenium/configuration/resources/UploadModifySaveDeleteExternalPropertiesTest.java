ee2100c264585289f7655be417e421d49ed0fd73
jm036438 <jeffrey.mahmood@cerner.com>
2016-12-01 16:44:34 -0500

    update selenium test to modify and save External Properties

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/configuration/resources/UploadModifySaveDeleteExternalPropertiesTest.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/configuration/resources/UploadModifySaveDeleteExternalPropertiesTest.java
@@ -4,15 +4,10 @@ import com.cerner.jwala.ui.selenium.SeleniumTestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 /**
  * Created on 11/14/2016.
@@ -42,7 +37,22 @@ public class UploadModifySaveDeleteExternalPropertiesTest extends SeleniumTestCa
         }
 
         driver.findElement(By.linkText("Configuration")).click();
+        for (int second = 0; ; second++) {
+            if (second >= 60) fail("timeout");
+            try {
+                if (isElementPresent(By.linkText("JVM")))
+                    break;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Thread.sleep(1000);
+        }
+
+        waitABit();
+
         driver.findElement(By.linkText("Resources")).click();
+
+        // upload an external properties file
         driver.findElement(By.xpath("//span[text()=\"Ext Properties\"]")).click();
         driver.findElement(By.cssSelector("span.ui-icon.ui-icon-plusthick")).click();
         driver.findElement(By.name("templateFile")).clear();
@@ -57,6 +67,7 @@ public class UploadModifySaveDeleteExternalPropertiesTest extends SeleniumTestCa
             Thread.sleep(1000);
         }
 
+        // modify the external properties file
         driver.findElement(By.xpath("//span[text()=\"ext.properties\"]")).click();
         driver.findElement(By.cssSelector("li.ui-state-active > span")).click();
         for (int second = 0; ; second++) {
@@ -68,33 +79,46 @@ public class UploadModifySaveDeleteExternalPropertiesTest extends SeleniumTestCa
             Thread.sleep(1000);
         }
 
-        assertTrue(driver.findElement(By.cssSelector(".CodeMirror-line > span")).isDisplayed());
-        assertTrue(driver.findElement(By.cssSelector(".CodeMirror-line > span")).isEnabled());
+        Actions actions = new Actions(driver);
+        driver.findElement(By.xpath("//pre")).click();
 
-        driver.findElement(By.cssSelector(".CodeMirror-line > span")).click();
-        driver.findElement(By.cssSelector(".CodeMirror-line > span")).sendKeys("selenium.test.property=running selenium tests ${vars['AemSsh.userName']}");
-        assertEquals("selenium.test.property=running selenium tests ${vars['AemSsh.userName']}", driver.findElement(By.cssSelector("pre..CodeMirror-line")).getText());
+        actions.sendKeys(Keys.HOME).build().perform();
+        actions.sendKeys(Keys.ENTER).build().perform();
+        actions.sendKeys(Keys.ARROW_UP).build().perform();
+        actions.sendKeys("selenium.test.property=running selenium tests ${vars['resources.enabled']}").build().perform();
+        assertEquals("selenium.test.property=running selenium tests ${vars['resources.enabled']}", driver.findElement(By.xpath("//pre")).getText());
+
         driver.findElement(By.cssSelector("span.ui-icon.ui-icon-disk")).click();
+        waitABit();
         driver.findElement(By.linkText("Template Preview")).click();
+        waitABit();
+        assertEquals("selenium.test.property=running selenium tests true", driver.findElement(By.xpath("//div[2]/div/div/div/div[6]/div/div/div/div/div[5]/div/pre")).getText());
+
+        // upload an external properties file again and check that the warning appears
         driver.findElement(By.xpath("//span[text()=\"Ext Properties\"]")).click();
+        waitABit();
         driver.findElement(By.cssSelector("span.ui-icon.ui-icon-plusthick")).click();
+        waitABit();
         assertEquals("Only one external properties file can be uploaded. Any existing ones will be overwritten.", driver.findElement(By.cssSelector("span.msg")).getText());
         driver.findElement(By.name("templateFile")).clear();
         driver.findElement(By.name("templateFile")).sendKeys("D:\\jwala-resources\\external.properties");
         driver.findElement(By.xpath("(//span[text()=\"Ok\"])")).click();
-        for (int second = 0; ; second++) {
-            if (second >= 60) fail("timeout");
-            try {
-                if (!isElementPresent(By.xpath("//input[@name='templateFile']"))) break;
-            } catch (Exception e) {
-            }
-            Thread.sleep(1000);
-        }
+        waitABit();
+        driver.findElement(By.xpath("//span[text()=\"ext.properties\"]")).click();
+        waitABit();
+        assertEquals("external.property.one=1", driver.findElement(By.xpath("//pre")).getText());
+        driver.findElement(By.linkText("Template Preview")).click();
+        waitABit();
+        assertEquals("external.property.one=1", driver.findElement(By.xpath("//div[2]/div/div/div/div[6]/div/div/div/div/div[5]/div/pre")).getText());
 
+        // delete the file
         driver.findElement(By.cssSelector("input.noSelect")).click();
+        waitABit();
         driver.findElement(By.cssSelector("span.ui-icon.ui-icon-trash")).click();
+        waitABit();
         driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
-        assertFalse(isElementPresent(By.xpath("//span[text()=\"ext.properties\"]")));
+        waitABit();
+        assertTrue(isElementPresent(By.xpath("//span[text()=\"No resources found...\"]")));
     }
 
     @After
