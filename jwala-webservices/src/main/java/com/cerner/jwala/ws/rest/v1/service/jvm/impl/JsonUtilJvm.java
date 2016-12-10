c7849d4675da6a99f579549f83ab695d813cfe8e
JC043760 <jedd.cuison@cerner.com>
2016-12-10 02:33:29 -0500

    Add private constructor since this is a utility class

--- jwala-webservices/src/main/java/com/cerner/jwala/ws/rest/v1/service/jvm/impl/JsonUtilJvm.java
+++ jwala-webservices/src/main/java/com/cerner/jwala/ws/rest/v1/service/jvm/impl/JsonUtilJvm.java
@@ -10,13 +10,16 @@ class JsonUtilJvm {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonUtilJvm.class);
 
+    private JsonUtilJvm() {
+    }
+
     /**
      * Convert a string to an Integer.
      * Return null if the string cannot be converted.
      * @param val the string value to convert
      * @return Integer. Null if conversion fails.
      */
-    static final Integer stringToInteger(final String val) {
+    static Integer stringToInteger(final String val) {
         try {
             return Integer.valueOf(val);
         } catch (NumberFormatException nfe) {
