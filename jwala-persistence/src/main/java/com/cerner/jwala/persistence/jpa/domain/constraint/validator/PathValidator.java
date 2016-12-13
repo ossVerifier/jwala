de7b99a06d94d32d310767b1e41b117740a9255a
JC043760 <jedd.cuison@cerner.com>
2016-12-13 17:38:53 -0500

    Do not do path validation when the value is empty

--- jwala-persistence/src/main/java/com/cerner/jwala/persistence/jpa/domain/constraint/validator/PathValidator.java
+++ jwala-persistence/src/main/java/com/cerner/jwala/persistence/jpa/domain/constraint/validator/PathValidator.java
@@ -34,6 +34,10 @@ public class PathValidator implements ConstraintValidator<ValidPath, String> {
     @Override
     public boolean isValid(final String value, final ConstraintValidatorContext context) {
         try {
+            if (StringUtils.isEmpty(value)) {
+                return true; // do not validate empty value there are other constraints for that e.g. @NotNull, @Size etc...
+            }
+
             final Path path = Paths.get(value);
 
             if (checkIfExists && !path.toFile().exists()) {
@@ -42,7 +46,7 @@ public class PathValidator implements ConstraintValidator<ValidPath, String> {
 
             final String fileExt = FilenameUtils.getExtension(value);
             return allowableFileExtensions.isEmpty() ||
-                   !allowableFileExtensions.stream().filter(validFileExt -> fileExt.equalsIgnoreCase(validFileExt)).findFirst().orElse(StringUtils.EMPTY).isEmpty();
+                   !allowableFileExtensions.stream().filter(fileExt::equalsIgnoreCase).findFirst().orElse(StringUtils.EMPTY).isEmpty();
 
         } catch (final InvalidPathException e) {
             LOGGER.error(e.getMessage(), e);
