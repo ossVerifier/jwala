adfccd2f093c144b5427343b87b7f3d1162f663f
Lin-Hung Wu <Lin-Hung.Wu@cerner.com>
2017-01-05 15:56:50 -0500

    SOARCOM-1912 Linux-Add service templates in jwala to install and start service on Linux

--- jwala-tomcat/src/main/resources/data/scripts/install-ws-service.sh
+++ jwala-tomcat/src/main/resources/data/scripts/install-ws-service.sh
@@ -7,28 +7,44 @@ JWALA_EXIT_CODE_NO_OP=127
 JWALA_EXIT_CODE_SUCCESS=0
 JWALA_EXIT_CODE_FAILED=1
 
-if [ "$1" = "" -o "$2" = "" ]; then
-    /usr/bin/echo $0 not invoked with service name or instances folder path
-    exit $JWALA_EXIT_CODE_NO_OP;
-fi
-export JVMINST=`sc queryex $1 | head -1 | awk '{ sub(/:/,"",$4); print $4 }'`
-if [ "$JVMINST" = "1060" ]; then
-    echo Service $1 not installed on server, continuing with invoke
-else
-    /usr/bin/echo Service $1 already exists
-    exit $JWALA_EXIT_CODE_FAILED
-fi
-$2/install_serviceWS.bat
+cygwin=false
+linux=false
+case "`uname`" in
+CYGWIN*) cygwin=true;;
+Linux*) linux=true;;
+esac
 
-for (( c=1; c<=5; c++ ))
-do
-    /usr/bin/sleep 1
-done
+if $cygwin; then
+  if [ "$1" = "" -o "$2" = "" ]; then
+      /usr/bin/echo $0 not invoked with service name or instances folder path
+      exit $JWALA_EXIT_CODE_NO_OP;
+  fi
+  export JVMINST=`sc queryex $1 | head -1 | awk '{ sub(/:/,"",$4); print $4 }'`
+  if [ "$JVMINST" = "1060" ]; then
+      echo Service $1 not installed on server, continuing with invoke
+  else
+      /usr/bin/echo Service $1 already exists
+      exit $JWALA_EXIT_CODE_FAILED
+  fi
+  $2/install_serviceWS.bat
 
-export JVMINST=`sc queryex $1 | head -1 | awk '{ sub(/:/,"",$4); print $4 }'`
-if [ "$JVMINST" = "1060" ]; then
-    /usr/bin/echo Failed to install service $1
-    exit $JWALA_EXIT_CODE_FAILED
+  for (( c=1; c<=5; c++ ))
+  do
+      /usr/bin/sleep 1
+  done
+
+  export JVMINST=`sc queryex $1 | head -1 | awk '{ sub(/:/,"",$4); print $4 }'`
+  if [ "$JVMINST" = "1060" ]; then
+      /usr/bin/echo Failed to install service $1
+      exit $JWALA_EXIT_CODE_FAILED
+  fi
+  /usr/bin/echo Invoke of service $1 was successful
+  exit $JWALA_EXIT_CODE_SUCCESS
 fi
-/usr/bin/echo Invoke of service $1 was successful
-exit $JWALA_EXIT_CODE_SUCCESS
+
+if $linux; then
+  # Need to pass $3 for apache home ex: /opt/ctp/apache-httpd-2.4.20, remote.paths.apache.httpd from vars.properties
+  sed -e "s/@APACHE_HOME@/${3//\//\\/}/g" -e "s/@HTTPD_CONF@/${2//\//\\/}\\/httpd.conf/g" /linux/httpd-ws-service> $1
+  chmod 755 $1
+  sudo cp $1 /etc/init.d
+fi
