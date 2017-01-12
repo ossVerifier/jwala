93b8273fda3a213867dc14e3401f13263e127e3d
JC043760 <jedd.cuison@cerner.com>
2017-01-12 14:57:01 -0500

    Replace Thread Local approach with SecurityContextHolder MODE_INHERITABLETHREADLOCAL strategy implementation to get the user from the persistence layer
    https://jira2.cerner.com/browse/SOARCOM-1906

--- jwala-common/src/main/java/com/cerner/jwala/common/domain/model/user/User.java
+++ jwala-common/src/main/java/com/cerner/jwala/common/domain/model/user/User.java
@@ -3,16 +3,10 @@ package com.cerner.jwala.common.domain.model.user;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
 
 public class User implements Serializable {
-
-    private static final ThreadLocal<User> USER_THREAD_LOCAL = new ThreadLocal<User>();
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(User.class);
 
     public static User getSystemUser() {
         return new User("systemUser");
@@ -26,18 +20,6 @@ public class User implements Serializable {
 
     public String getId() {
         return id;
-    }
-
-    public void addToThread() {
-        User.USER_THREAD_LOCAL.set(this);
-    }
-
-    public static User getThreadLocalUser() {
-        return User.USER_THREAD_LOCAL.get();
-    }
-
-    public void invalidate() {
-        User.USER_THREAD_LOCAL.set(null);
     }
 
     @Override
@@ -70,4 +52,5 @@ public class User implements Serializable {
                 .append("id", id)
                 .toString();
     }
+
 }
