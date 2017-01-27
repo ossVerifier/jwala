ef615b4962789dc974311e1c91afe5d77ee77b06
JC043760 <jedd.cuison@cerner.com>
2017-01-27 13:31:50 -0500

    Write unit test and clean up code
    https://jira2.cerner.com/browse/SOARCOM-1906

--- jwala-services/src/main/java/com/cerner/jwala/service/webserver/WebServerStateRetrievalScheduledTaskHandler.java
+++ jwala-services/src/main/java/com/cerner/jwala/service/webserver/WebServerStateRetrievalScheduledTaskHandler.java
@@ -1,6 +1,5 @@
 package com.cerner.jwala.service.webserver;
 
-import com.cerner.jwala.common.domain.model.id.Identifier;
 import com.cerner.jwala.common.domain.model.webserver.WebServer;
 import com.cerner.jwala.service.webserver.component.WebServerStateSetterWorker;
 
@@ -8,10 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Future;
 
 /**
  * Handles periodic retrieval of all web server states. This handler is defined as a Spring bean in conjunction
@@ -23,24 +19,19 @@ public class WebServerStateRetrievalScheduledTaskHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebServerStateRetrievalScheduledTaskHandler.class);
     private final WebServerService webServerService;
     private final WebServerStateSetterWorker webServerStateSetterWorker;
-    private final Map<Identifier<WebServer>, Future<?>> webServerFutureMap;
     private boolean enabled;
 
     public WebServerStateRetrievalScheduledTaskHandler(final WebServerService webServerService,
-                                                       final WebServerStateSetterWorker webServerStateSetterWorker,
-                                                       final Map<Identifier<WebServer>, Future<?>> webServerFutureMap) {
+                                                       final WebServerStateSetterWorker webServerStateSetterWorker) {
         this.webServerService = webServerService;
         this.webServerStateSetterWorker = webServerStateSetterWorker;
-        this.webServerFutureMap = webServerFutureMap;
     }
 
     public WebServerStateRetrievalScheduledTaskHandler(final WebServerService webServerService,
                                                        final WebServerStateSetterWorker webServerStateSetterWorker,
-                                                       final Map<Identifier<WebServer>, Future<?>> webServerFutureMap,
                                                        final boolean enabled) {
         this.webServerService = webServerService;
         this.webServerStateSetterWorker = webServerStateSetterWorker;
-        this.webServerFutureMap = webServerFutureMap;
         this.enabled = enabled;
     }
 
@@ -57,34 +48,6 @@ public class WebServerStateRetrievalScheduledTaskHandler {
             webServerStateSetterWorker.pingWebServer(webServer);
         }
 
-    }
-
-    /**
-     * Remove Futures whose key does not match any of the web server ids in the web server list.
-     *
-     * @param webServers the web server list
-     */
-    private void cleanFuturesMap(final List<WebServer> webServers) {
-        final List<Identifier<WebServer>> futureKeysForRemoval = new ArrayList<>();
-        if (webServers.size() < webServerFutureMap.size()) {
-            for (Identifier<WebServer> key : webServerFutureMap.keySet()) {
-                boolean hasMatch = false;
-                for (WebServer webServer : webServers) {
-                    hasMatch = key.equals(webServer.getId());
-                    if (hasMatch) {
-                        break;
-                    }
-                }
-
-                if (!hasMatch) {
-                    futureKeysForRemoval.add(key);
-                }
-            }
-
-            for (Identifier<WebServer> key : futureKeysForRemoval) {
-                webServerFutureMap.remove(key);
-            }
-        }
     }
 
     public boolean isEnabled() {
