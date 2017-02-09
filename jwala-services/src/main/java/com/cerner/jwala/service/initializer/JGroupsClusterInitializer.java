5a9c8cebd2dab088079a27b554a51cdf19692a3b
jm036438 <jeffrey.mahmood@cerner.com>
2017-02-09 15:04:29 -0500

    get IP address for JGroup coordinator by hostname
    Signed-off-by: jm036438 <jeffrey.mahmood@cerner.com>

--- jwala-services/src/main/java/com/cerner/jwala/service/initializer/JGroupsClusterInitializer.java
+++ jwala-services/src/main/java/com/cerner/jwala/service/initializer/JGroupsClusterInitializer.java
@@ -2,7 +2,6 @@ package com.cerner.jwala.service.initializer;
 
 import com.cerner.jwala.common.properties.ApplicationProperties;
 import com.cerner.jwala.service.exception.JGroupsClusterInitializerException;
-
 import org.jgroups.Event;
 import org.jgroups.JChannel;
 import org.jgroups.PhysicalAddress;
@@ -12,6 +11,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+
 /**
  * JGroups cluster initializer.
  * <p/>
@@ -19,9 +23,12 @@ import org.springframework.beans.factory.InitializingBean;
  */
 public class JGroupsClusterInitializer implements InitializingBean {
     private static final Logger LOGGER = LoggerFactory.getLogger(JGroupsClusterInitializer.class);
+    private static final String JGROUPS_COORDINATOR_HOSTNAME = "jgroups.coordinator.hostname";
+    private static final String JGROUPS_COORDINATOR_IP_ADDRESS = "jgroups.coordinator.ip.address";
 
     private final String jgroupsJavaNetPreferIPv4Stack = ApplicationProperties.get("jgroups.java.net.preferIPv4Stack", "true");
-    private final String jgroupsCoordinatorIPAddress = ApplicationProperties.get("jgroups.coordinator.ip.address");
+    private final String jgroupsCoordinatorHostname = ApplicationProperties.get(JGROUPS_COORDINATOR_HOSTNAME);
+    private final String jgroupsCoordinatorIPAddress = ApplicationProperties.get(JGROUPS_COORDINATOR_IP_ADDRESS);
     private final String jgroupsCoordinatorPort = ApplicationProperties.get("jgroups.coordinator.port");
     private final String jgroupsClusterConnectTimeout = ApplicationProperties.get("jgroups.cluster.connect.timeout", "10000");
     private final String jgroupsClusterName = ApplicationProperties.get("jgroups.cluster.name", "DefaultJwalaCluster");
@@ -41,7 +48,17 @@ public class JGroupsClusterInitializer implements InitializingBean {
             final JChannel channel = new JChannel(jgroupsConfXml);
             channel.setReceiver(receiverAdapter);
 
-            IpAddress coordinatorIP = new IpAddress(this.jgroupsCoordinatorIPAddress + ":" + jgroupsCoordinatorPort);
+            IpAddress coordinatorIP;
+            if (null != this.jgroupsCoordinatorIPAddress) {
+                coordinatorIP = new IpAddress(this.jgroupsCoordinatorIPAddress + ":" + jgroupsCoordinatorPort);
+            } else {
+                if (null == this.jgroupsCoordinatorHostname) {
+                    final String errorMessage = MessageFormat.format("Expecting hostname specified as property {0} when no IP address specified for the JGroups coordinator", JGROUPS_COORDINATOR_HOSTNAME);
+                    LOGGER.error(errorMessage);
+                    throw new JGroupsClusterInitializerException(errorMessage, null);
+                }
+                coordinatorIP = getIPAddressFromHostname(jgroupsCoordinatorHostname);
+            }
             channel.connect(jgroupsClusterName, coordinatorIP, Long.parseLong(jgroupsClusterConnectTimeout));
             LOGGER.debug("JGroups connection to cluster {} SUCCESSFUL", jgroupsClusterName);
 
@@ -51,5 +68,32 @@ public class JGroupsClusterInitializer implements InitializingBean {
             LOGGER.error("FAILURE using JGroups: could not connect to cluster {}", jgroupsClusterName, e);
             throw new JGroupsClusterInitializerException("JGroups cluster initialization failed!", e);
         }
+    }
+
+    private IpAddress getIPAddressFromHostname(final String jgroupsCoordinatorHostname) throws UnknownHostException {
+        final InetAddress[] allByName = InetAddress.getAllByName(jgroupsCoordinatorHostname);
+        if (null == allByName || 0 == allByName.length){
+            final String errorMessage = MessageFormat.format("Expecting at least 1 IP address from hostname {0}, but were returned none", jgroupsCoordinatorHostname);
+            LOGGER.error(errorMessage);
+            throw new JGroupsClusterInitializerException(errorMessage, null);
+        }
+
+        LOGGER.info("Getting JGroups IP address by hostname {} returned a total of {} addresses", jgroupsCoordinatorHostname, allByName.length);
+        ArrayList<InetAddress> filteredAddresses = new ArrayList<>();
+        for (InetAddress address : allByName) {
+            LOGGER.debug("Filtering JGroups IP address {}", address);
+            if (!address.isLoopbackAddress()){
+                LOGGER.debug("-- adding JGroups IP address to list: {}", address);
+                filteredAddresses.add(address);
+            }
+        }
+
+        if (filteredAddresses.size() != 1) {
+            final String errorMessage = MessageFormat.format("Expecting only 1 filtered JGroups IP address, but instead found {0}. Please use the IP address property {1} to specify the JGroups coordinator node location.", filteredAddresses.size(), JGROUPS_COORDINATOR_IP_ADDRESS);
+            LOGGER.error(errorMessage);
+            throw new JGroupsClusterInitializerException(errorMessage, null);
+        }
+
+        return new IpAddress(filteredAddresses.get(0), Integer.parseInt(jgroupsCoordinatorPort));
     }
 }
