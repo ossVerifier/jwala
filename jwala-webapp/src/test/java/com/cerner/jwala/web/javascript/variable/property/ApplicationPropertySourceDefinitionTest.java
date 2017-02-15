a677715aca96a0ba1dee6b772b9a83676816df2f
JC043760 <jedd.cuison@cerner.com>
2017-02-15 03:59:36 -0500

    Fix failing test

--- jwala-webapp/src/test/java/com/cerner/jwala/web/javascript/variable/property/ApplicationPropertySourceDefinitionTest.java
+++ jwala-webapp/src/test/java/com/cerner/jwala/web/javascript/variable/property/ApplicationPropertySourceDefinitionTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static com.cerner.jwala.web.javascript.variable.property.ApplicationPropertySourceDefinition.LOAD_BALANCER_STATUS_MOUNT;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for {@link ApplicationPropertySourceDefinition}
@@ -16,7 +17,7 @@ public class ApplicationPropertySourceDefinitionTest {
         assertEquals("/balancer-manager", LOAD_BALANCER_STATUS_MOUNT.getDefaultValue());
         assertEquals("mod_jk.load-balancer.status.mount", LOAD_BALANCER_STATUS_MOUNT.getPropertyKey());
         assertEquals("loadBalancerStatusMount", LOAD_BALANCER_STATUS_MOUNT.getVariableName());
-        assertEquals("com.cerner.jwala.web.javascript.variable.property.ApplicationPropertySourceDefinition@28f67ac7[variableName=loadBalancerStatusMount,propertyKey=mod_jk.load-balancer.status.mount,defaultValue=/balancer-manager]", LOAD_BALANCER_STATUS_MOUNT.toString());
+        assertTrue(LOAD_BALANCER_STATUS_MOUNT.toString().contains("[variableName=loadBalancerStatusMount,propertyKey=mod_jk.load-balancer.status.mount,defaultValue=/balancer-manager]"));
     }
 
 }
