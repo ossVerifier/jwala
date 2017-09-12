6cbb0f454c71f4e647c0695dfc65b26fafd64477
Jedd Cuison <jedd.cuison@cerner.com>
2017-09-12 02:00:21 -0400

    Check for the login page

--- jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/LoginRunSteps.java
+++ jwala-integration-test/src/test/java/com/cerner/jwala/ui/selenium/steps/LoginRunSteps.java
@@ -23,6 +23,7 @@ public class LoginRunSteps {
     @Given("^I am on the login page$")
     public void loadLoginPage() {
         jwalaUi.loadPath("/login");
+        jwalaUi.waitUntilElementIsVisible(By.id("logInForm"));
     }
 
     @When("^I fill in the \"User Name\" field with a valid user name$")
